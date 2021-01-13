6b2956f3ee635f91495580969c7229dcac8e451a
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-01-13 18:39:35 +0500

    Update services

--- src/main/java/ru/example/todo/service/impl/TaskServiceImpl.java
+++ src/main/java/ru/example/todo/service/impl/TaskServiceImpl.java
@@ -4,5 +4,13 @@ package ru.example.todo.service.impl;
  * Time: 6:16 PM
  * */
 
-public class TaskServiceImpl {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import ru.example.todo.service.TaskService;
+
+@Service
+public class TaskServiceImpl implements TaskService {
+
+    private static final Logger log = LoggerFactory.getLogger(TaskServiceImpl.class.getName());
 }
--- src/main/java/ru/example/todo/service/impl/TodoSectionImpl.java
+++ src/main/java/ru/example/todo/service/impl/TodoSectionImpl.java
@@ -4,5 +4,13 @@ package ru.example.todo.service.impl;
  * Time: 6:16 PM
  * */
 
-public class TodoSectionImpl {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import ru.example.todo.service.TodoSectionService;
+
+@Service
+public class TodoSectionImpl implements TodoSectionService {
+
+    private static final Logger log = LoggerFactory.getLogger(TodoSectionImpl.class.getName());
 }
