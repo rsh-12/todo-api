9646ab1808d029adee0dad8f6252036ffef8e275
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-01-13 21:56:23 +0500

    Implement get one and get all methods

--- src/main/java/ru/example/todo/controller/TodoTaskController.java
+++ src/main/java/ru/example/todo/controller/TodoTaskController.java
@@ -6,8 +6,23 @@ package ru.example.todo.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.example.todo.controller.assemblers.TodoTaskModelAssembler;
+import ru.example.todo.exception.TodoTaskNotFoundException;
+import ru.example.todo.entity.TodoTask;
+import ru.example.todo.service.TodoTaskService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/api/todos")
@@ -15,5 +30,30 @@ public class TodoTaskController {
 
     private static final Logger log = LoggerFactory.getLogger(TodoTaskController.class.getName());
 
+    private final TodoTaskService todoTaskService;
+    private final TodoTaskModelAssembler assembler;
 
+    @Autowired
+    public TodoTaskController(TodoTaskService todoTaskService, TodoTaskModelAssembler assembler) {
+        this.todoTaskService = todoTaskService;
+        this.assembler = assembler;
+    }
+
+
+    @GetMapping(produces = "application/json")
+    public CollectionModel<EntityModel<TodoTask>> all() {
+
+        List<EntityModel<TodoTask>> todos = todoTaskService.getListOfAllTodos().stream()
+                .map(assembler::toModel)
+                .collect(Collectors.toList());
+
+        return CollectionModel.of(todos, linkTo(methodOn(TodoTaskController.class).all()).withSelfRel());
+    }
+
+    @GetMapping("/{id}")
+    public EntityModel<TodoTask> one(@PathVariable("id") Long id) {
+        TodoTask todo = todoTaskService.getOne(id).orElseThrow(() ->
+                new TodoTaskNotFoundException(id));
+        return assembler.toModel(todo);
+    }
 }
