1affa1849f640c45e142006f60442902d8531cc8
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-01-14 20:16:50 +0500

    Add delete task method

--- src/main/java/ru/example/todo/controller/TodoTaskController.java
+++ src/main/java/ru/example/todo/controller/TodoTaskController.java
@@ -9,13 +9,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import ru.example.todo.controller.assembler.TodoTaskModelAssembler;
-import ru.example.todo.exception.TodoObjectNotFoundException;
 import ru.example.todo.entity.TodoTask;
+import ru.example.todo.exception.TodoObjectNotFoundException;
 import ru.example.todo.service.TodoTaskService;
 
 import java.util.List;
@@ -33,13 +31,14 @@ public class TodoTaskController {
     private final TodoTaskService todoTaskService;
     private final TodoTaskModelAssembler assembler;
 
+
     @Autowired
     public TodoTaskController(TodoTaskService todoTaskService, TodoTaskModelAssembler assembler) {
         this.todoTaskService = todoTaskService;
         this.assembler = assembler;
     }
 
-
+    // get all tasks
     @GetMapping(produces = "application/json")
     public CollectionModel<EntityModel<TodoTask>> all() {
 
@@ -47,14 +46,23 @@ public class TodoTaskController {
                 .map(assembler::toModel)
                 .collect(Collectors.toList());
 
-        return CollectionModel.of(todos, linkTo(methodOn(TodoTaskController.class).all()).withSelfRel());
+        return CollectionModel.of(todos,
+                linkTo(methodOn(TodoTaskController.class).all()).withSelfRel());
     }
 
-    @GetMapping("/{id}")
+    // get task by id
+    @GetMapping(value = "/{id}", produces = "application/json")
     public EntityModel<TodoTask> one(@PathVariable("id") Long id) {
         TodoTask todo = todoTaskService.getTaskById(id).orElseThrow(() ->
-                new TodoObjectNotFoundException("TodoTask not found: " + id));
-
+                new TodoObjectNotFoundException("Task not found: " + id));
         return assembler.toModel(todo);
+    }
+
+    // delete task by id
+    @DeleteMapping("/{id}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void deleteOne(@PathVariable Long id) {
+        if (todoTaskService.existsById(id))
+            todoTaskService.deleteTaskById(id);
     }
 }
