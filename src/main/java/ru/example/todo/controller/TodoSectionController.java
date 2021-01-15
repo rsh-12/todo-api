538dfdc5994c6bf9ffd5f0c780e6de7a0ea57e3f
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-01-15 16:41:20 +0500

    Add get all and get by id methods

--- src/main/java/ru/example/todo/controller/TodoSectionController.java
+++ src/main/java/ru/example/todo/controller/TodoSectionController.java
@@ -5,19 +5,51 @@ package ru.example.todo.controller;
  * */
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.example.todo.controller.assembler.TodoSectionModelAssembler;
+import ru.example.todo.entity.TodoSection;
 import ru.example.todo.service.TodoSectionService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("api/section")
 public class TodoSectionController {
 
     private final TodoSectionService todoSectionService;
+    private final TodoSectionModelAssembler assembler;
 
     @Autowired
-    public TodoSectionController(TodoSectionService todoSectionService) {
+    public TodoSectionController(TodoSectionService todoSectionService, TodoSectionModelAssembler assembler) {
         this.todoSectionService = todoSectionService;
+        this.assembler = assembler;
     }
+
+    // get all custom sections
+    @GetMapping
+    public CollectionModel<EntityModel<TodoSection>> all() {
+        List<EntityModel<TodoSection>> sections = todoSectionService.getAllSections().stream()
+                .map(assembler::toModel)
+                .collect(Collectors.toList());
+
+        return CollectionModel.of(sections,
+                linkTo(methodOn(TodoSectionController.class).all()).withSelfRel());
+    }
+
+    // get custom section by id
+    @GetMapping(value = "/{id}", consumes = "application/json")
+    public EntityModel<TodoSection> one(@PathVariable("id") Long id) {
+        return assembler.toModel(todoSectionService.getSectionById(id));
+    }
+
 
 }
