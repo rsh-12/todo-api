2c3d5a5c45d1173ac19bea1ef7f4a3afbaebced8
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-01-15 13:28:18 +0500

    Add new converter

---
+++ src/main/java/ru/example/todo/util/StringToDateEnumConverter.java
@@ -0,0 +1,15 @@
+package ru.example.todo.util;
+/*
+ * Date: 1/15/21
+ * Time: 11:45 AM
+ * */
+
+import org.springframework.core.convert.converter.Converter;
+import ru.example.todo.enums.TaskDate;
+
+public class StringToDateEnumConverter implements Converter<String, TaskDate> {
+    @Override
+    public TaskDate convert(String string) {
+        return TaskDate.valueOf(string.toUpperCase());
+    }
+}
