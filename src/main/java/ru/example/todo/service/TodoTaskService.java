6252f5526f4ab95e1699cd984ed489fa5f8650f9
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-01-15 09:55:16 +0500

    Add setTaskStatus method

--- src/main/java/ru/example/todo/service/TodoTaskService.java
+++ src/main/java/ru/example/todo/service/TodoTaskService.java
@@ -1,6 +1,7 @@
 package ru.example.todo.service;
 
 import ru.example.todo.entity.TodoTask;
+import ru.example.todo.enums.TaskStatus;
 
 import java.util.List;
 
@@ -12,9 +13,9 @@ public interface TodoTaskService {
 
     void deleteTaskById(Long id);
 
-    void save(TodoTask task);
-
     void createTask(TodoTask newTask);
 
     void updateTask(TodoTask patch, Long id);
+
+    void setTaskStatus(Long taskId, TaskStatus completed, TaskStatus starred);
 }
