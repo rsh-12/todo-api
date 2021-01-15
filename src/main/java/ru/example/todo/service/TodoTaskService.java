ac62472959ceb1f04982a840acfc53792b6b14e9
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-01-15 13:26:40 +0500

    Update method args

--- src/main/java/ru/example/todo/service/TodoTaskService.java
+++ src/main/java/ru/example/todo/service/TodoTaskService.java
@@ -1,13 +1,14 @@
 package ru.example.todo.service;
 
 import ru.example.todo.entity.TodoTask;
+import ru.example.todo.enums.TaskDate;
 import ru.example.todo.enums.TaskStatus;
 
 import java.util.List;
 
 public interface TodoTaskService {
 
-    List<TodoTask> getAllTasks();
+    List<TodoTask> getAllTasks(Integer pageNo, Integer pageSize, TaskDate date, String sort);
 
     TodoTask getTaskById(Long id);
 
