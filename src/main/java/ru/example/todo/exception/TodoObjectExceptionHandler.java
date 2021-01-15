11a6a68c95c113fb7aac29befea5450b5decfdd4
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-01-15 09:52:17 +0500

    Add conversion exception handler

--- src/main/java/ru/example/todo/exception/TodoObjectExceptionHandler.java
+++ src/main/java/ru/example/todo/exception/TodoObjectExceptionHandler.java
@@ -4,6 +4,7 @@ package ru.example.todo.exception;
  * Time: 10:42 AM
  * */
 
+import org.springframework.core.convert.ConversionFailedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -32,6 +33,17 @@ public class TodoObjectExceptionHandler {
         var error = new CustomErrorResponse();
         error.setStatus(HttpStatus.BAD_REQUEST.value());
         error.setMessage("Something went wrong");
+        error.setTimestamp(System.currentTimeMillis());
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ConversionFailedException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ResponseEntity<CustomErrorResponse> handleException(RuntimeException ex) {
+
+        var error = new CustomErrorResponse();
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage("Conversation error");
         error.setTimestamp(System.currentTimeMillis());
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
