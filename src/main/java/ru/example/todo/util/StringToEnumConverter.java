58ab8b7dd0776e2bc606d550a1601a4e60c29129
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-01-15 09:55:58 +0500

    Add to enum converter

---
+++ src/main/java/ru/example/todo/util/StringToEnumConverter.java
@@ -0,0 +1,16 @@
+package ru.example.todo.util;
+/*
+ * Date: 1/15/21
+ * Time: 8:45 AM
+ * */
+
+import org.springframework.core.convert.converter.Converter;
+import ru.example.todo.enums.TaskStatus;
+
+public class StringToEnumConverter implements Converter<String, TaskStatus> {
+
+    @Override
+    public TaskStatus convert(String string) {
+        return TaskStatus.valueOf(string.toUpperCase());
+    }
+}
