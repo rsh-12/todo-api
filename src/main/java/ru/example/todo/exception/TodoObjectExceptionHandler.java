ffd24357d1f2637ee5130714bdfd3ec239945ac1
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-01-16 18:02:43 +0500

    Add field errors handler static method

--- src/main/java/ru/example/todo/exception/TodoObjectExceptionHandler.java
+++ src/main/java/ru/example/todo/exception/TodoObjectExceptionHandler.java
@@ -7,11 +7,15 @@ package ru.example.todo.exception;
 import org.springframework.core.convert.ConversionFailedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class TodoObjectExceptionHandler {
@@ -53,5 +57,15 @@ public class TodoObjectExceptionHandler {
         error.setMessage("Conversion error");
 
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    public static Map<String, String> getFieldErrorsHandler(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
     }
 }
