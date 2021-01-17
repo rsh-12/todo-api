d89ef085a28de58d98a9579bd74cbd02e511491d
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-01-17 17:38:52 +0500

    Update title min size

--- src/main/java/ru/example/todo/entity/TodoSection.java
+++ src/main/java/ru/example/todo/entity/TodoSection.java
@@ -26,7 +26,7 @@ public class TodoSection {
     private Long id;
 
     @NotBlank
-    @Size(min = 2, max = 50, message = "Size must be between 2 and 50")
+    @Size(min = 3, max = 50, message = "Size must be between 3 and 50")
     @Column(name = "title")
     @JsonView(value = Views.Public.class)
     private String title;
