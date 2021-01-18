ce0ba922aa3d9b33ad369525c6d8c71510cd107d
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-01-18 16:23:56 +0500

    Remove unnecessary exception handler

--- src/main/java/ru/example/todo/exception/TodoObjectExceptionHandler.java
+++ src/main/java/ru/example/todo/exception/TodoObjectExceptionHandler.java
@@ -7,18 +7,15 @@ package ru.example.todo.exception;
 import org.springframework.core.convert.ConversionFailedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 @ControllerAdvice
-public class TodoObjectExceptionHandler {
+public class TodoObjectExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
@@ -59,13 +56,4 @@ public class TodoObjectExceptionHandler {
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
-    public static Map<String, String> getFieldErrorsHandler(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-        return errors;
-    }
 }
