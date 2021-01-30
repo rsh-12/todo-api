f0a912ad81238bde17ecb591888d30a07e4da616
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-01-30 16:27:06 +0500

    Add api info

--- src/main/java/ru/example/todo/config/SwaggerConfig.java
+++ src/main/java/ru/example/todo/config/SwaggerConfig.java
@@ -8,9 +8,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
@@ -22,6 +26,18 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("ru.example.todo"))
                 .paths(PathSelectors.ant("/api/**"))
-                .build();
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo("ToDo tasks API",
+                "Study project",
+                "1.1",
+                "Free to use",
+                new Contact("RSH-12", "https://github.com/rsh-12", "get_info@bk.ru"),
+                null,
+                null,
+                Collections.emptyList());
     }
 }
