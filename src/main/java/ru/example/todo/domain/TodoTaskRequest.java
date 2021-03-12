1b7b1040e4f7fc3f1fd721e5e4deebd4a63a2f49
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-03-12 19:18:27 +0500

    Add request class for the todoTask entity

--- src/main/java/ru/example/todo/domain/TodoTaskRequest.java
+++ src/main/java/ru/example/todo/domain/TodoTaskRequest.java
@@ -4,6 +4,41 @@ package ru.example.todo.domain;
  * Time: 6:52 PM
  * */
 
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+
 public class TodoTaskRequest {
 
+    @Size(min = 3, max = 80, message = "Size must be between 3 and 80")
+    private String title;
+
+
+    @FutureOrPresent
+    private LocalDate completionDate = LocalDate.now();
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+    public LocalDate getCompletionDate() {
+        return completionDate;
+    }
+
+    public void setCompletionDate(LocalDate completionDate) {
+        this.completionDate = completionDate;
+    }
+
+    @Override
+    public String toString() {
+        return "TodoTaskRequest{" +
+                "title='" + title + '\'' +
+                ", completionDate=" + completionDate +
+                '}';
+    }
 }
