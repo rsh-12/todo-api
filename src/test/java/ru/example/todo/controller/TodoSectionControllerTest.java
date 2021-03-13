64607f408255d46a3e47240cfc61c47c71de81b2
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-03-13 14:38:08 +0500

    Add createSection test case

--- src/test/java/ru/example/todo/controller/TodoSectionControllerTest.java
+++ src/test/java/ru/example/todo/controller/TodoSectionControllerTest.java
@@ -5,8 +5,8 @@ package ru.example.todo.controller;
  * */
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -19,6 +19,7 @@ import ru.example.todo.repository.TodoSectionRepository;
 
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -43,7 +44,6 @@ public class TodoSectionControllerTest {
 
     // get all sections
     @Test
-    @DisplayName("Test /api/sections/ returns list of sections and 200 OK")
     public void testGetAllTodoSections() throws Exception {
 
         createSection();
@@ -52,13 +52,11 @@ public class TodoSectionControllerTest {
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("_embedded.sections[0].title", is("TestSection")))
-                .andDo(print())
-                .andReturn();
+                .andDo(print());
     }
 
     // get section by ID
     @Test
-    @DisplayName("Test /api/sections/1 returns the section by ID 1 and 200 OK")
     public void testGetTodoSectionById() throws Exception {
 
         createSection();
@@ -66,14 +64,36 @@ public class TodoSectionControllerTest {
         mvc.perform(get(API + 1))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("title", is("TestSection")))
-                .andDo(print())
-                .andReturn();
+                .andDo(print());
     }
 
     // create new section
+
+    @Test
+    public void testCreateNewSection() throws Exception {
+
+        TodoSection section = new TodoSection();
+        section.setTitle("CreatedSection");
+
+        mvc.perform(post(API)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(section)))
+                .andExpect(status().isCreated())
+                .andDo(print());
+    }
+
 
     // delete section
 
     // update section
 
+    private static String asJsonString(final Object obj) {
+        try {
+            final ObjectMapper mapper = new ObjectMapper();
+            return mapper.writeValueAsString(obj);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Error during converting");
+        }
+    }
 }
