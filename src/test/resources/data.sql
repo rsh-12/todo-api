a656eff22604bd061a52f23a0b3ac615f22f8cc5
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-03-14 08:42:18 +0500

    Update DML scripts

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,11 +1,16 @@
-insert into custom_list(title)
-values ('Important'),
-       ('Starred'),
-       ('Later');
+insert into section(id, title, created_at, updated_at)
+values (1, 'Important', current_timestamp, current_timestamp),
+       (2, 'Starred', current_timestamp, current_timestamp),
+       (3, 'Later', current_timestamp, current_timestamp);
 
 
-insert into task(title)
-values ('Read a book'),
-       ('Create a presentation'),
-       ('Write a letter');
+insert into task(id, title, completed, starred, completion_date, created_at, updated_at, list_id)
+values (1, 'Read a book', false, false, current_timestamp,
+        current_timestamp, current_timestamp, 1),
+
+       (2, 'Create a presentation', false, false, current_timestamp,
+        current_timestamp, current_timestamp, 2),
+
+       (3, 'Write a letter', false, false, current_timestamp,
+        current_timestamp, current_timestamp, 3);
 
