5e8d4c3be22e12e0df332fd42c9164d74c41c6fb
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-03-14 19:20:03 +0500

    Update getAllTasks, add getTaskById test methods

--- src/test/java/ru/example/todo/controller/TodoTaskControllerTest.java
+++ src/test/java/ru/example/todo/controller/TodoTaskControllerTest.java
@@ -7,22 +7,43 @@ package ru.example.todo.controller;
 import org.junit.Test;
 import org.springframework.http.MediaType;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /*
  * Test data:
  * Read a book, Create a presentation, Write a letter
  * */
-
 public class TodoTaskControllerTest extends AbstractTestContollerClass {
 
     @Test
-    public void A_testGetAllTasks() throws Exception {
+    public void testGetAllTasks_WithAndWithoutParams() throws Exception {
+        /*
+         * param "day" == completionDate
+         * */
         mvc.perform(get(TASKS)
                 .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        mvc.perform(get(TASKS)
+                .contentType(MediaType.APPLICATION_JSON)
+                .param("date", "today"))
+                .andExpect(status().isOk());
+
+        mvc.perform(get(TASKS)
+                .contentType(MediaType.APPLICATION_JSON)
+                .param("date", "overdue"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testGetTaskById() throws Exception {
+        final int TASK_ID = 1;
+
+        mvc.perform(get(TASKS + TASK_ID))
                 .andExpect(status().isOk())
-                .andDo(print());
+                .andExpect(jsonPath("id", is(TASK_ID)));
     }
 }
