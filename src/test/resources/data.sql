f49896b66e054bf7622733ee43791190c38d42be
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-03-14 09:01:26 +0500

    Fix data types

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,16 +1,16 @@
 insert into section(id, title, created_at, updated_at)
-values (1, 'Important', current_timestamp, current_timestamp),
-       (2, 'Starred', current_timestamp, current_timestamp),
-       (3, 'Later', current_timestamp, current_timestamp);
+values (1, 'Important', current_timestamp, DATEADD('HOUR', 1, current_timestamp)),
+       (2, 'Starred', DATEADD('DAY', -7, current_timestamp), DATEADD('DAY', -2, current_timestamp)),
+       (3, 'Later', DATEADD('MONTH', -1, current_timestamp), DATEADD('WEEK', -1, current_timestamp));
 
 
 insert into task(id, title, completed, starred, completion_date, created_at, updated_at, list_id)
 values (1, 'Read a book', false, false, current_timestamp,
-        current_timestamp, current_timestamp, 1),
+        current_timestamp, DATEADD('MINUTE', 40, current_timestamp), 1),
 
        (2, 'Create a presentation', false, false, current_timestamp,
-        current_timestamp, current_timestamp, 2),
+        DATEADD('MONTH', -2, current_timestamp), current_timestamp, 2),
 
        (3, 'Write a letter', false, false, current_timestamp,
-        current_timestamp, current_timestamp, 3);
+        DATEADD('WEEK', -3, current_timestamp), DATEADD('WEEK', -1, current_timestamp), 3);
 
