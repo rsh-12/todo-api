93698f9a7bd7b14d67e7be618145778a94b6c49d
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-03-14 19:18:14 +0500

    Update completion date

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -5,10 +5,10 @@ values (1, 'Important', current_timestamp, DATEADD('HOUR', 1, current_timestamp)
 
 
 insert into task(id, title, completed, starred, completion_date, created_at, updated_at, list_id)
-values (1, 'Read a book', false, false, current_timestamp,
+values (1, 'Read a book', false, false, DATEADD('WEEK', +1, current_timestamp),
         current_timestamp, DATEADD('MINUTE', 40, current_timestamp), 1),
 
-       (2, 'Create a presentation', false, true, current_timestamp,
+       (2, 'Create a presentation', false, true, DATEADD('WEEK', -1, current_timestamp),
         DATEADD('WEEK', -3, current_timestamp), DATEADD('DAY', -3, current_timestamp), 2),
 
        (3, 'Write a letter', true, false, current_timestamp,
