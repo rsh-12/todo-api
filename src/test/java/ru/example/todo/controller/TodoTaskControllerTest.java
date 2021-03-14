2f6ffe0d6077a11e5105bd3d8095d9a9a6f8c45c
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-03-14 07:30:53 +0500

    Create TodoTaskControllerTest class

---
+++ src/test/java/ru/example/todo/controller/TodoTaskControllerTest.java
@@ -0,0 +1,23 @@
+package ru.example.todo.controller;
+/*
+ * Date: 3/14/21
+ * Time: 7:17 AM
+ * */
+
+import org.junit.Test;
+import org.springframework.http.MediaType;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class TodoTaskControllerTest extends AbstractTestContollerClass {
+
+    @Test
+    public void A_testGetAllTasks() throws Exception {
+        mvc.perform(get(TASKS)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+}
