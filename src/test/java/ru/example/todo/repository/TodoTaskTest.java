92ff2562cb97095ec64fbf9a60d95bc2fa5429f4
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-03-15 20:17:05 +0500

    Add getAllTasks test mehod

--- src/test/java/ru/example/todo/repository/TodoTaskTest.java
+++ src/test/java/ru/example/todo/repository/TodoTaskTest.java
@@ -4,11 +4,18 @@ package ru.example.todo.repository;
  * Time: 9:45 AM
  * */
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
+import ru.example.todo.entity.TodoTask;
+import ru.example.todo.exception.TodoObjectException;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -19,4 +26,17 @@ public class TodoTaskTest {
 
     @Autowired
     private TestEntityManager entityManager;
+
+    // get all
+    @Test
+    public void getAllTasks() {
+        List<TodoTask> tasks = repository.findAll();
+        assertEquals(6, tasks.size());
+    }
+
+
+    // create one
+    // update by id
+    // delete by id
+    // z_delete all*
 }
