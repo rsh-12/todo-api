1551b44b4d0c79bb81604d558ec85087a27057e7
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-03-25 13:32:49 +0500

    Implement scheduled task

---
+++ src/main/java/ru/example/todo/util/ClearTokenStoreScheduledTask.java
@@ -0,0 +1,27 @@
+package ru.example.todo.util;
+/*
+ * Date: 3/25/21
+ * Time: 1:24 PM
+ * */
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import ru.example.todo.service.TokenStore;
+
+@Component
+public class ClearTokenStoreScheduledTask {
+
+    private final TokenStore tokenStore;
+
+    @Autowired
+    public ClearTokenStoreScheduledTask(TokenStore tokenStore) {
+        this.tokenStore = tokenStore;
+    }
+
+    // Delete expired refresh tokens from memory every day at 01:00 a.m.
+    @Scheduled(cron = "0 0 1 * * ?")
+    public void clearTokenStore() {
+        tokenStore.removeIfExpired();
+    }
+}
