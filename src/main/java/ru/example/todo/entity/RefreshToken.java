a9e3f25632b1a39d33253d0de662bd8e49be0ce4
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-03-25 15:34:22 +0500

    Implement Builder pattern

--- src/main/java/ru/example/todo/entity/RefreshToken.java
+++ src/main/java/ru/example/todo/entity/RefreshToken.java
@@ -11,38 +11,55 @@ import static java.time.Instant.now;
 
 public class RefreshToken {
 
-    private String id;
-
-    private String username;
-
-    private Date expiryTime;
+    private final String id;
+    private final String username;
+    private final Date expiryTime;
 
     private long getValiditySeconds() {
         return expiryTime == null ? Long.MAX_VALUE : Duration.between(now(), expiryTime.toInstant()).getSeconds();
+    }
+
+    public static class Builder {
+        private String id;
+        private String username;
+        private Date expiryTime;
+
+        public Builder id(String val) {
+            id = val;
+            return this;
+        }
+
+        public Builder username(String val) {
+            username = val;
+            return this;
+        }
+
+        public Builder expiryTime(Date val) {
+            expiryTime = val;
+            return this;
+        }
+
+        public RefreshToken build() {
+            return new RefreshToken(this);
+        }
+    }
+
+    private RefreshToken(Builder builder) {
+        id = builder.id;
+        username = builder.username;
+        expiryTime = builder.expiryTime;
     }
 
     public String getId() {
         return id;
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
     public String getUsername() {
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
     public Date getExpiryTime() {
         return expiryTime;
-    }
-
-    public void setExpiryTime(Date expiryTime) {
-        this.expiryTime = expiryTime;
     }
 
     @Override
@@ -53,4 +70,5 @@ public class RefreshToken {
                 ", expiryTime=" + expiryTime +
                 '}';
     }
+
 }
