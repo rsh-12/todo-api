f5b6b023186bb758edd9f76d43f31d0180da58f8
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-03-25 13:30:37 +0500

    Update ProjectConfig class

--- src/main/java/ru/example/todo/config/ProjectConfig.java
+++ src/main/java/ru/example/todo/config/ProjectConfig.java
@@ -7,13 +7,39 @@ package ru.example.todo.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import ru.example.todo.service.TokenStore;
+import ru.example.todo.service.impl.InMemoryTokenStore;
+import ru.example.todo.util.StringToDateEnumConverter;
+import ru.example.todo.util.StringToSetTasksEnumConverter;
+import ru.example.todo.util.StringToStatusEnumConverter;
 
 @Configuration
-public class ProjectConfig {
+public class ProjectConfig implements WebMvcConfigurer {
 
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new InMemoryTokenStore();
+    }
+
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new StringToStatusEnumConverter());
+        registry.addConverter(new StringToDateEnumConverter());
+        registry.addConverter(new StringToSetTasksEnumConverter());
     }
 
 }
