991608f7d395728d7c259a6538d2531820a12e35
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-03-25 13:31:54 +0500

    Rename class

---
+++ src/main/java/ru/example/todo/config/WebSecurityConfig.java
@@ -0,0 +1,24 @@
+package ru.example.todo.config;
+/*
+ * Date: 1/15/21
+ * Time: 8:47 AM
+ * */
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+}
