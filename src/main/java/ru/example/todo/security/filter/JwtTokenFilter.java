f76fd69cb92d43e4889e5c68c21bb48ee4656738
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-03-25 15:33:10 +0500

    Update JwtTokenFilter

--- src/main/java/ru/example/todo/security/filter/JwtTokenFilter.java
+++ src/main/java/ru/example/todo/security/filter/JwtTokenFilter.java
@@ -4,5 +4,46 @@ package ru.example.todo.security.filter;
  * Time: 1:04 PM
  * */
 
-public class JwtTokenFilter {
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+import ru.example.todo.exception.CustomException;
+import ru.example.todo.service.JwtTokenService;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtTokenFilter extends OncePerRequestFilter {
+
+    private final JwtTokenService jwtTokenService;
+
+    public JwtTokenFilter(JwtTokenService jwtTokenService) {
+        this.jwtTokenService = jwtTokenService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest,
+                                    HttpServletResponse httpServletResponse,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        String token = jwtTokenService.resolveToken(httpServletRequest);
+
+        try {
+            if (token != null && jwtTokenService.validateToken(token)) {
+                Authentication auth = jwtTokenService.getAuthentication(token);
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        } catch (CustomException ex) {
+            SecurityContextHolder.clearContext();
+            httpServletResponse.sendError(HttpStatus.BAD_REQUEST.value(), "Invalied token");
+            return;
+        }
+
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+
 }
