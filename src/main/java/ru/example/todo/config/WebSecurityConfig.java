0286520e52a0e371fc73e8582111f8ac4aed54f5
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-03-25 15:34:48 +0500

    Override http config method

--- src/main/java/ru/example/todo/config/WebSecurityConfig.java
+++ src/main/java/ru/example/todo/config/WebSecurityConfig.java
@@ -4,21 +4,43 @@ package ru.example.todo.config;
  * Time: 8:47 AM
  * */
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import ru.example.todo.security.JwtTokenFilterConfigurer;
+import ru.example.todo.service.JwtTokenService;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private JwtTokenService jwtTokenService;
+
     @Bean
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.authorizeRequests()
+                .mvcMatchers("/api/users/login").permitAll()
+                .mvcMatchers("/api/users/register").permitAll()
+                .anyRequest().authenticated();
+
+        http.apply(new JwtTokenFilterConfigurer(jwtTokenService));
     }
 }
