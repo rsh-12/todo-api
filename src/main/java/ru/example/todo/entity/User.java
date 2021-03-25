0ed16eb3e0e81b025d76be4f64f654ca858daffd
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-03-25 10:18:06 +0500

    Add User entity

---
+++ src/main/java/ru/example/todo/entity/User.java
@@ -0,0 +1,99 @@
+package ru.example.todo.entity;
+/*
+ * Date: 3/25/21
+ * Time: 9:47 AM
+ * */
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+import java.util.Date;
+import java.util.Set;
+
+@Entity
+@Table(name = "app_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Email
+    @Size(min = 4, max = 127, message = "Email is required: minimum 4 characters")
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Size(min = 8, message = "Password is required")
+    private String password;
+
+    @JsonFormat(timezone = "Asia/Yekaterinburg")
+    @CreationTimestamp
+    private Date createdAt;
+
+    @JsonFormat(timezone = "Asia/Yekaterinburg")
+    @UpdateTimestamp
+    private Date updateAt;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private Set<Role> roles;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Date updateAt) {
+        this.updateAt = updateAt;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                '}';
+    }
+}
