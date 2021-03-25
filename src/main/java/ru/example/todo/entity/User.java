3c8e8d6f37ca06abc7d6b7a892c41282769600ae
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-03-25 11:02:18 +0500

    Add task, sections fields, getter/setters

--- src/main/java/ru/example/todo/entity/User.java
+++ src/main/java/ru/example/todo/entity/User.java
@@ -5,13 +5,14 @@ package ru.example.todo.entity;
  * */
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Size;
 import java.util.Date;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -27,6 +28,7 @@ public class User {
     @Column(nullable = false, unique = true)
     private String username;
 
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @Size(min = 8, message = "Password is required")
     private String password;
 
@@ -34,12 +36,17 @@ public class User {
     @CreationTimestamp
     private Date createdAt;
 
-    @JsonFormat(timezone = "Asia/Yekaterinburg")
-    @UpdateTimestamp
-    private Date updateAt;
-
     @ElementCollection(fetch = FetchType.EAGER)
     private Set<Role> roles;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Set<TodoSection> todoSections;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<TodoTask> todoTasks;
+
+    public User() {
+    }
 
     public Long getId() {
         return id;
@@ -73,20 +80,36 @@ public class User {
         this.createdAt = createdAt;
     }
 
-    public Date getUpdateAt() {
-        return updateAt;
-    }
-
-    public void setUpdateAt(Date updateAt) {
-        this.updateAt = updateAt;
-    }
-
     public Set<Role> getRoles() {
         return roles;
     }
 
     public void setRoles(Set<Role> roles) {
         this.roles = roles;
+    }
+
+    public void addRole(Role role) {
+        roles.add(role);
+    }
+
+    public void removeRole(Role role) {
+        roles.remove(role);
+    }
+
+    public List<TodoTask> getTodoTasks() {
+        return todoTasks;
+    }
+
+    public void setTodoTasks(List<TodoTask> todoTasks) {
+        this.todoTasks = todoTasks;
+    }
+
+    public Set<TodoSection> getTodoSections() {
+        return todoSections;
+    }
+
+    public void setTodoSections(Set<TodoSection> todoSections) {
+        this.todoSections = todoSections;
     }
 
     @Override
