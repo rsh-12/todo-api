084bef5ad305164ddd35538870bed3eb8068a37c
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-03-25 16:40:55 +0500

    Implement JwtTokenService

--- src/main/java/ru/example/todo/service/impl/JwtTokenServiceImpl.java
+++ src/main/java/ru/example/todo/service/impl/JwtTokenServiceImpl.java
@@ -4,62 +4,146 @@ package ru.example.todo.service.impl;
  * Time: 1:05 PM
  * */
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import ru.example.todo.config.properties.TokenProperties;
 import ru.example.todo.entity.RefreshToken;
 import ru.example.todo.entity.Role;
+import ru.example.todo.exception.CustomException;
+import ru.example.todo.repository.UserRepository;
+import ru.example.todo.security.UserDetailsServiceImpl;
 import ru.example.todo.service.JwtTokenService;
+import ru.example.todo.service.TokenStore;
 
+import javax.annotation.PostConstruct;
+import javax.crypto.SecretKey;
 import javax.servlet.http.HttpServletRequest;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import static java.time.Instant.now;
 
 @Service
 public class JwtTokenServiceImpl implements JwtTokenService {
 
+    private final UserDetailsServiceImpl userDetailsService;
     private final TokenProperties tokenProperties;
+    private final UserRepository userRepository;
+    private final TokenStore tokenStore;
 
-    public JwtTokenServiceImpl(TokenProperties tokenProperties) {
+    private SecretKey secretKey;
+
+    public JwtTokenServiceImpl(TokenProperties tokenProperties, UserDetailsServiceImpl userDetailsService, UserRepository userRepository, TokenStore tokenStore) {
         this.tokenProperties = tokenProperties;
+        this.userDetailsService = userDetailsService;
+        this.userRepository = userRepository;
+        this.tokenStore = tokenStore;
+    }
+
+    @PostConstruct
+    protected void init() {
+        secretKey = Keys.hmacShaKeyFor(tokenProperties.getSecret().getBytes(StandardCharsets.UTF_8));
     }
 
     @Override
     public String buildAccessToken(String username, Set<Role> roles) {
-        return null;
+
+        Claims claims = Jwts.claims().setSubject(username);
+        claims.put("auth", roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getAuthority()))
+                .collect(Collectors.toList()));
+
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + tokenProperties.getAccessTokenValidity());
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setExpiration(validity)
+                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .setHeaderParam("e-x-p", validity)
+                .compact();
     }
 
     @Override
-    public String buildAccessToken(String username) {
-        return null;
+    public RefreshToken buildRefreshToken(String username) {
+
+        if (!userRepository.existsByUsername(username)) {
+            throw new UsernameNotFoundException("User not found: " + username);
+        }
+
+        Date validity = new Date(System.currentTimeMillis() + tokenProperties.getRefreshTokenValidity());
+
+        RefreshToken refreshToken = new RefreshToken.Builder()
+                .id(RandomStringUtils.randomAlphanumeric(64))
+                .expiryTime(validity)
+                .username(username)
+                .build();
+
+        return tokenStore.save(refreshToken);
     }
+
 
     @Override
     public Authentication getAuthentication(String token) {
-        return null;
+        UserDetails userDetails = userDetailsService.loadUserByUsername(getUsername(token));
+        return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
     }
 
     @Override
     public String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
         return null;
     }
 
     @Override
     public boolean validateToken(String token) {
-        return false;
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(secretKey).build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException ex) {
+            throw new CustomException("Expired or invalid JWT token", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @Override
     public RefreshToken findRefreshTokenById(String tokenId) {
-        return null;
+        return tokenStore.findById(tokenId);
     }
 
     @Override
     public void removeOldRefreshTokenById(String tokenId) {
-
+        tokenStore.deleteById(tokenId);
     }
 
     @Override
     public boolean isValidRefreshToken(RefreshToken refreshToken) {
-        return false;
+        return !now().isAfter(refreshToken.getExpiryTime().toInstant());
+    }
+
+    private String getUsername(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody().getSubject();
     }
 }
