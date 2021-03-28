9ab7d9284dde6ed23d6c6500bccaf4d793580545
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-03-28 20:09:10 +0500

    Add new query methods

--- src/main/java/ru/example/todo/repository/TodoSectionRepository.java
+++ src/main/java/ru/example/todo/repository/TodoSectionRepository.java
@@ -3,5 +3,14 @@ package ru.example.todo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.example.todo.entity.TodoSection;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface TodoSectionRepository extends JpaRepository<TodoSection, Long> {
+
+    List<TodoSection> findAllByUserId(Long id);
+
+    Optional<TodoSection> findByUserIdAndId(Long userId, Long sectionId);
+
+    void deleteByIdAndUserId(Long sectionId, Long userId);
 }
