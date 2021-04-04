58963831cbbd11af4ecf15675097fadea9632225
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-04-04 08:41:11 +0500

    Add get all by user query method

--- src/main/java/ru/example/todo/repository/TodoSectionRepository.java
+++ src/main/java/ru/example/todo/repository/TodoSectionRepository.java
@@ -2,6 +2,7 @@ package ru.example.todo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.example.todo.entity.TodoSection;
+import ru.example.todo.entity.User;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,6 +10,8 @@ import java.util.Optional;
 public interface TodoSectionRepository extends JpaRepository<TodoSection, Long> {
 
     List<TodoSection> findAllByUserId(Long id);
+
+    List<TodoSection> findAllByUser(User user);
 
     Optional<TodoSection> findByUserIdAndId(Long userId, Long sectionId);
 
