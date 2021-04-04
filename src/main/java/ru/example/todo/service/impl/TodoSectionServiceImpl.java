5fe28b90b7b1fe11c8dba0621df6c3aa984dfe9b
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-04-04 09:57:37 +0500

    Replace UserDetailsImpl with User

--- src/main/java/ru/example/todo/service/impl/TodoSectionServiceImpl.java
+++ src/main/java/ru/example/todo/service/impl/TodoSectionServiceImpl.java
@@ -7,7 +7,6 @@ package ru.example.todo.service.impl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import ru.example.todo.dto.TodoSectionDto;
 import ru.example.todo.entity.Role;
@@ -17,8 +16,6 @@ import ru.example.todo.entity.User;
 import ru.example.todo.enums.SetTasks;
 import ru.example.todo.exception.CustomException;
 import ru.example.todo.repository.TodoSectionRepository;
-import ru.example.todo.repository.UserRepository;
-import ru.example.todo.security.UserDetailsImpl;
 import ru.example.todo.service.TodoSectionService;
 import ru.example.todo.service.TodoTaskService;
 
@@ -32,28 +29,26 @@ public class TodoSectionServiceImpl implements TodoSectionService {
 
     private final TodoSectionRepository todoSectionRepository;
     private final TodoTaskService todoTaskService;
-    private final UserRepository userRepository;
 
     public TodoSectionServiceImpl(TodoSectionRepository todoSectionRepository,
-                                  TodoTaskService todoTaskService, UserRepository userRepository) {
+                                  TodoTaskService todoTaskService) {
         this.todoSectionRepository = todoSectionRepository;
         this.todoTaskService = todoTaskService;
-        this.userRepository = userRepository;
     }
 
     // get section by id
     @Override
-    public TodoSection getSectionById(UserDetailsImpl uds, Long sectionId) {
+    public TodoSection getSectionById(User user, Long sectionId) {
         log.info("Get the section by id: {}", sectionId);
-        return todoSectionRepository.findByUserIdAndId(uds.getId(), sectionId)
+        return todoSectionRepository.findByUserIdAndId(user.getId(), sectionId)
                 .orElseThrow(() -> new CustomException("Section not found: " + sectionId, HttpStatus.NOT_FOUND));
     }
 
     // get all sections
     @Override
-    public List<TodoSection> getAllSections(UserDetailsImpl uds) {
+    public List<TodoSection> getAllSections(User user) {
 
-        List<TodoSection> sections = todoSectionRepository.findAllByUserId(uds.getId());
+        List<TodoSection> sections = todoSectionRepository.findAllByUserId(user.getId());
         log.info("Get all sections: {}", sections.size());
         return sections;
     }
@@ -61,20 +56,18 @@ public class TodoSectionServiceImpl implements TodoSectionService {
     // todo uds
     // delete section by id
     @Override
-    public void deleteSectionById(UserDetailsImpl uds, Long sectionId) {
+    public void deleteSectionById(User user, Long sectionId) {
 
         TodoSection section = todoSectionRepository.findById(sectionId)
                 .orElseThrow(() -> new CustomException("Section not found", HttpStatus.NOT_FOUND));
 
-        validateArgs(uds, section);
+        validateArgs(user, section);
     }
 
 
     // create new section
     @Override
-    public void createSection(TodoSectionDto sectionDto, UserDetailsImpl uds) {
-
-        User user = uds.getUser();
+    public void createSection(User user, TodoSectionDto sectionDto) {
 
         TodoSection section = new TodoSection();
         section.setUser(user);
@@ -86,45 +79,33 @@ public class TodoSectionServiceImpl implements TodoSectionService {
 
     // update section title
     @Override
-    public void updateSection(Long userId, Long sectionId, TodoSectionDto sectionDto) {
+    public void updateSection(User user, Long sectionId, TodoSectionDto sectionDto) {
 
         // get a section by id
         TodoSection section = todoSectionRepository.findById(sectionId)
                 .orElseThrow(() -> new CustomException("Section not found: " + sectionId, HttpStatus.NOT_FOUND));
 
-        validateArgs(userId, sectionDto, section);
+        validateArgs(user, sectionDto, section);
 
         todoSectionRepository.save(section);
     }
 
 
-    private void validateArgs(Long userId, TodoSectionDto sectionDto, TodoSection section) {
-        if (section.getUser() != null && section.getUser().getId().equals(userId)) {
+    private void validateArgs(User user, TodoSectionDto sectionDto, TodoSection section) {
+
+        if ((section.getUser() != null && section.getUser().equals(user)) || user.getRoles().contains(Role.ROLE_ADMIN)) {
             section.setTitle(sectionDto.getTitle());
         } else {
-            User user = userRepository.findById(userId)
-                    .orElseThrow(() -> new CustomException("User not found", HttpStatus.INTERNAL_SERVER_ERROR));
-
-            if (user.getRoles().contains(Role.ROLE_ADMIN)) {
-                section.setTitle(sectionDto.getTitle());
-            } else {
-                throw new CustomException("Forbidden", HttpStatus.FORBIDDEN);
-            }
+            throw new CustomException("Not enough permissions", HttpStatus.FORBIDDEN);
         }
     }
 
-    private void validateArgs(UserDetailsImpl uds, TodoSection section) {
+    private void validateArgs(User user, TodoSection section) {
 
-        User user = uds.getUser();
-
-        if (section.getUser() != null && section.getUser().equals(user)) {
+        if ((section.getUser() != null && section.getUser().equals(user)) || user.getRoles().contains(Role.ROLE_ADMIN)) {
             todoSectionRepository.delete(section);
         } else {
-            if (user.getRoles().contains(Role.ROLE_ADMIN)) {
-                todoSectionRepository.delete(section);
-            } else {
-                throw new CustomException("Forbidden", HttpStatus.FORBIDDEN);
-            }
+            throw new CustomException("Not enough permissions", HttpStatus.FORBIDDEN);
         }
     }
 
