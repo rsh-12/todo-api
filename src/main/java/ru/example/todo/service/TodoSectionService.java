3f1ab411ea224fc10203b4238260b3232871a5c2
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-04-04 08:41:58 +0500

    Update methods: replace userId with UserDetailsImpl

--- src/main/java/ru/example/todo/service/TodoSectionService.java
+++ src/main/java/ru/example/todo/service/TodoSectionService.java
@@ -3,19 +3,20 @@ package ru.example.todo.service;
 import ru.example.todo.dto.TodoSectionDto;
 import ru.example.todo.entity.TodoSection;
 import ru.example.todo.enums.SetTasks;
+import ru.example.todo.security.UserDetailsImpl;
 
 import java.util.List;
 import java.util.Set;
 
 public interface TodoSectionService {
 
-    TodoSection getSectionById(Long userId, Long sectionId);
+    TodoSection getSectionById(UserDetailsImpl uds, Long sectionId);
 
-    List<TodoSection> getAllSections(Long userId);
+    List<TodoSection> getAllSections(UserDetailsImpl uds);
 
-    void deleteSectionById(Long userId, Long sectionId);
+    void deleteSectionById(UserDetailsImpl uds, Long sectionId);
 
-    void createSection(TodoSectionDto sectionDto, Long userId);
+    void createSection(TodoSectionDto sectionDto, UserDetailsImpl uds);
 
     void updateSection(Long userId, Long sectionId, TodoSectionDto sectionDto);
 
