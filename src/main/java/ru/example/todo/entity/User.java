a253082a6ee5d99d4ff5d304148693e630986e9b
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-04-04 16:45:54 +0500

    Add equals/hashcode

--- src/main/java/ru/example/todo/entity/User.java
+++ src/main/java/ru/example/todo/entity/User.java
@@ -122,6 +122,24 @@ public class User {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (id != null ? !id.equals(user.id) : user.id != null) return false;
+        return username != null ? username.equals(user.username) : user.username == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (username != null ? username.hashCode() : 0);
+        return result;
+    }
+
+    @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
