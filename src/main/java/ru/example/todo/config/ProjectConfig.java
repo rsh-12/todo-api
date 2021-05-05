714e9aa4bfddf9c4eafb030579a191af69609654
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-05-05 12:12:50 +0500

    Add some jackson beans

--- src/main/java/ru/example/todo/config/ProjectConfig.java
+++ src/main/java/ru/example/todo/config/ProjectConfig.java
@@ -4,18 +4,24 @@ package ru.example.todo.config;
  * Time: 10:29 AM
  * */
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.modelmapper.ModelMapper;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import ru.example.todo.service.TokenStore;
 import ru.example.todo.service.impl.InMemoryTokenStore;
-import ru.example.todo.util.StringToDateEnumConverter;
-import ru.example.todo.util.StringToSetTasksEnumConverter;
-import ru.example.todo.util.StringToStatusEnumConverter;
+import ru.example.todo.util.converters.StringToDateEnumConverter;
+import ru.example.todo.util.converters.StringToSetTasksEnumConverter;
+import ru.example.todo.util.converters.StringToStatusEnumConverter;
+
+import java.text.SimpleDateFormat;
 
 @Configuration
 public class ProjectConfig implements WebMvcConfigurer {
@@ -48,5 +54,21 @@ public class ProjectConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("*");
+    }
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer customDeserialization() {
+        return jacksonObjectMapperBuilder -> {
+            jacksonObjectMapperBuilder.featuresToDisable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+            jacksonObjectMapperBuilder.modules(new Hibernate5Module());
+        };
+    }
+
+    @Bean
+    public Jackson2ObjectMapperBuilder jacksonBuilder() {
+        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
+        builder.indentOutput(true);
+        builder.dateFormat(new SimpleDateFormat("yyyy-MM-dd"));
+        return builder;
     }
 }
