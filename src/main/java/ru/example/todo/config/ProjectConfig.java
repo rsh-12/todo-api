95cd83665889a1500cbb00f8c23a0245bbab59b8
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-05-07 11:41:20 +0500

    Update modelMapper bean

--- src/main/java/ru/example/todo/config/ProjectConfig.java
+++ src/main/java/ru/example/todo/config/ProjectConfig.java
@@ -5,6 +5,7 @@ package ru.example.todo.config;
  * */
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
@@ -19,11 +20,6 @@ import ru.example.todo.util.converters.StringToStatusEnumConverter;
 
 @Configuration
 public class ProjectConfig implements WebMvcConfigurer {
-
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
-    }
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -48,6 +44,16 @@ public class ProjectConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("*");
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setSkipNullEnabled(true)
+                .setMatchingStrategy(MatchingStrategies.STRICT);
+
+        return modelMapper;
     }
 
 }
