d21e2adbec279b2f542a550b6a03571df47bc4c0
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-05-08 20:13:07 +0500

    Update SwaggerConfig
    I've implemented JWT authorization

--- src/main/java/ru/example/todo/config/SwaggerConfig.java
+++ src/main/java/ru/example/todo/config/SwaggerConfig.java
@@ -4,16 +4,19 @@ package ru.example.todo.config;
  * Time: 3:38 PM
  * */
 
+import com.google.inject.internal.util.Lists;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
 import java.util.Collections;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -21,17 +24,38 @@ public class SwaggerConfig {
     @Bean
     public Docket apiDocket() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .ignoredParameterTypes(AuthenticationPrincipal.class)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("ru.example.todo"))
                 .paths(PathSelectors.ant("/api/**"))
                 .build()
+                .securitySchemes(Lists.newArrayList(apiKey()))
+                .securityContexts(Lists.newArrayList(securityContext()))
                 .apiInfo(apiInfo());
+    }
+
+    @Bean
+    SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        var authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Lists.newArrayList(new SecurityReference("JWT", authorizationScopes));
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
     }
 
     private ApiInfo apiInfo() {
         return new ApiInfo("ToDo tasks API",
                 "Study project",
-                "1.2",
+                "1.3",
                 "Free to use",
                 new Contact("RSH-12", "https://github.com/rsh-12", "get_info@bk.ru"),
                 null,
