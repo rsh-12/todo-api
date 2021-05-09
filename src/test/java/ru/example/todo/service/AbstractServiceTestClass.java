f5b3ed6a32bc7562710fb16d650d4fe3ed76ea23
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-05-09 09:43:58 +0500

    Add AbstractServiceTestClass

---
+++ src/test/java/ru/example/todo/service/AbstractServiceTestClass.java
@@ -0,0 +1,14 @@
+package ru.example.todo.service;
+/*
+ * Date: 5/9/21
+ * Time: 9:38 AM
+ * */
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AbstractServiceTestClass {
+}
