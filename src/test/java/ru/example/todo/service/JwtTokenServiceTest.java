c01a98a528c2fa3cc8a549fb0da843066d0409bf
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-05-09 15:45:33 +0500

    Add buildAccessToken test method

--- src/test/java/ru/example/todo/service/JwtTokenServiceTest.java
+++ src/test/java/ru/example/todo/service/JwtTokenServiceTest.java
@@ -8,10 +8,13 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Import;
+import ru.example.todo.enums.Role;
 import ru.example.todo.exception.CustomException;
 import ru.example.todo.service.config.JwtTokenServiceImplTestConfig;
 
-import static org.junit.Assert.assertThrows;
+import java.util.Collections;
+
+import static org.junit.Assert.*;
 
 @Import(JwtTokenServiceImplTestConfig.class)
 public class JwtTokenServiceTest extends AbstractServiceTestClass {
@@ -23,5 +26,15 @@ public class JwtTokenServiceTest extends AbstractServiceTestClass {
     @Test
     public void validateToken_ShouldThrowException() throws CustomException {
         assertThrows(CustomException.class, () -> tokenService.validateToken("invalid"));
+    }
+
+    @Test
+    public void buildAccessToken_ShouldReturnAccessToken() throws CustomException {
+        String accessToken = tokenService
+                .buildAccessToken("admin", Collections.singleton(Role.ROLE_ADMIN));
+
+        int parts = accessToken.split("\\.").length;
+        assertEquals(3, parts);
+        System.out.println("accessToken = " + accessToken);
     }
 }
