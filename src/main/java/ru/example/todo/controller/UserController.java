4e452f4ff13f2f7770853baab37d38aede1bd405
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-05-15 18:52:52 +0500

    Move auth method into AuthController

--- src/main/java/ru/example/todo/controller/UserController.java
+++ src/main/java/ru/example/todo/controller/UserController.java
@@ -5,59 +5,34 @@ package ru.example.todo.controller;
  * */
 
 import io.swagger.annotations.Api;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-import ru.example.todo.dto.UserDto;
 import ru.example.todo.entity.User;
 import ru.example.todo.service.UserService;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
 
 @Api(tags = "Users")
 @RestController
 @RequestMapping("/api/users")
+@PreAuthorize("hasRole('ADMIN')")
 public class UserController {
 
     private final UserService userService;
-    private final ModelMapper modelMapper;
 
     @Autowired
-    public UserController(UserService userService, ModelMapper modelMapper) {
+    public UserController(UserService userService) {
         this.userService = userService;
-        this.modelMapper = modelMapper;
     }
 
     @GetMapping("/{id}")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public User getUser(@PathVariable("id") Long userId) {
         return userService.getUser(userId);
     }
 
-    @PostMapping(value = "/login", produces = "application/json")
-    public ResponseEntity<String> login(@Valid @RequestBody UserDto userDto) {
-        String tokens = userService.login(userDto);
-        return ResponseEntity.ok(tokens);
-    }
-
-    @PostMapping(value = "/register", produces = "application/json")
-    public ResponseEntity<String> register(@Valid @RequestBody UserDto userDto) {
-        String register = userService.register(modelMapper.map(userDto, User.class));
-        return ResponseEntity.ok(register);
-    }
-
-    @PostMapping(value = "/token", produces = "application/json")
-    public ResponseEntity<String> refreshToken(HttpServletRequest request) {
-        String tokens = userService.refreshToken(request.getHeader("token"));
-        return ResponseEntity.ok(tokens);
-    }
 
     @DeleteMapping(value = "/{id}")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<?> deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
