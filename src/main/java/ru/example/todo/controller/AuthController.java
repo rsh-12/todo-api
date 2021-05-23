a59c5cb153ec0d277e98e160b8dd1fd3a531ddb2
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-05-23 11:08:46 +0500

    Delete createAndSendOtp, resetPassword methods

--- src/main/java/ru/example/todo/controller/AuthController.java
+++ src/main/java/ru/example/todo/controller/AuthController.java
@@ -4,7 +4,6 @@ package ru.example.todo.controller;
  * Time: 6:10 PM
  * */
 
-import com.fasterxml.jackson.databind.JsonNode;
 import io.swagger.annotations.Api;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
@@ -12,10 +11,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.example.todo.domain.request.PasswordRequest;
 import ru.example.todo.dto.UserDto;
 import ru.example.todo.entity.User;
-import ru.example.todo.service.OtpService;
 import ru.example.todo.service.UserService;
 
 import javax.servlet.http.HttpServletRequest;
@@ -27,13 +24,10 @@ import javax.validation.Valid;
 public class AuthController {
 
     private final UserService userService;
-    private final OtpService otpService;
     private final ModelMapper modelMapper;
 
-    public AuthController(UserService userService,
-                          OtpService otpService, ModelMapper modelMapper) {
+    public AuthController(UserService userService, ModelMapper modelMapper) {
         this.userService = userService;
-        this.otpService = otpService;
         this.modelMapper = modelMapper;
     }
 
@@ -53,18 +47,6 @@ public class AuthController {
     public ResponseEntity<String> refreshToken(HttpServletRequest request) {
         String tokens = userService.refreshToken(request.getHeader("token"));
         return ResponseEntity.ok(tokens);
-    }
-
-    @PostMapping(value = "/password/forgot")
-    public ResponseEntity<?> createAndSendOtp(@RequestBody JsonNode body) {
-        otpService.sendOtp(body);
-        return ResponseEntity.ok("Code sent to mail successfully");
-    }
-
-    @PostMapping(value = "/password/reset", consumes = "application/json")
-    public ResponseEntity<?> resetPassword(@Valid @RequestBody PasswordRequest passwordRequest) {
-        userService.updatePassword(passwordRequest);
-        return ResponseEntity.ok().build();
     }
 
 }
