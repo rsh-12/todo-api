0e7cd65257e14cb2b62405b912d74978003a6c62
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-05-30 09:24:52 +0500

    Add token request pojo

---
+++ src/main/java/ru/example/todo/messaging/requests/Token.java
@@ -0,0 +1,32 @@
+package ru.example.todo.messaging.requests;
+/*
+ * Date: 5/29/21
+ * Time: 10:39 PM
+ * */
+
+public class Token {
+
+    private String token;
+
+    public Token() {
+    }
+
+    public Token(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    @Override
+    public String toString() {
+        return "Token{" +
+                "token='" + token + '\'' +
+                '}';
+    }
+}
