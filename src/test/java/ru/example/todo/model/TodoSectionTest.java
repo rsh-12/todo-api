478ca68be288a00fce9b9dc3a07e7f2de7ee4fcd
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-06-03 10:43:03 +0500

    Add TodoSectionTest class

---
+++ src/test/java/ru/example/todo/model/TodoSectionTest.java
@@ -0,0 +1,41 @@
+package ru.example.todo.model;
+/*
+ * Date: 6/3/21
+ * Time: 9:59 AM
+ * */
+
+import org.junit.Before;
+import org.junit.Test;
+import ru.example.todo.entity.TodoSection;
+import ru.example.todo.entity.TodoTask;
+
+import java.time.LocalDate;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class TodoSectionTest {
+
+    private TodoSection section;
+    private final TodoTask task = new TodoTask("First task", LocalDate.of(2021, 5, 5));
+
+    @Before
+    public void setUp() throws Exception {
+        section = new TodoSection("Important");
+        section.addTask(task);
+        assertEquals(1, section.getTodoTasks().size());
+    }
+
+    @Test
+    public void addTask_ShouldAddTaskToSection() {
+        section.addTask(new TodoTask("Make a call"));
+        assertEquals(2, section.getTodoTasks().size());
+    }
+
+    @Test
+    public void removeTask_ShouldRemoveTask() {
+        section.removeTask(task);
+        assertTrue(section.getTodoTasks().isEmpty());
+    }
+
+}
