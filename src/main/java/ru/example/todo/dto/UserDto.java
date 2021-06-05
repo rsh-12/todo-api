48ef8a189ae64a729641336fece410dbfc07e893
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-06-05 11:00:58 +0500

    Override equals and hashcode

--- src/main/java/ru/example/todo/dto/UserDto.java
+++ src/main/java/ru/example/todo/dto/UserDto.java
@@ -27,6 +27,32 @@ public class UserDto {
     @NotEmpty
     private String password;
 
+    public UserDto() {
+    }
+
+    public UserDto(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserDto userDto = (UserDto) o;
+
+        if (username != null ? !username.equals(userDto.username) : userDto.username != null) return false;
+        return password != null ? password.equals(userDto.password) : userDto.password == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = username != null ? username.hashCode() : 0;
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        return result;
+    }
+
     public String getUsername() {
         return username;
     }
