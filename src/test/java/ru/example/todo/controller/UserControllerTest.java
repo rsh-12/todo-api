674e4c817ead93f9230902128851a9d1bf8941c1
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-06-12 23:26:22 +0500

    Add getUser(200) test method

--- src/test/java/ru/example/todo/controller/UserControllerTest.java
+++ src/test/java/ru/example/todo/controller/UserControllerTest.java
@@ -5,16 +5,42 @@ package ru.example.todo.controller;
  * */
 
 import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithUserDetails;
+import ru.example.todo.entity.User;
+import ru.example.todo.service.UserService;
 
 import static org.hamcrest.Matchers.containsStringIgnoringCase;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-// todo update tests, mock service layer
+
 public class UserControllerTest extends AbstractControllerTestClass {
+
+    @MockBean
+    private UserService userService;
+
+    @Test
+    @WithUserDetails(ADMIN)
+    public void getUser_ShouldReturnUser() throws Exception {
+        given(userService.findUserById(Mockito.anyLong()))
+                .willReturn(new User("user", "password"));
+
+        mvc.perform(get(API_USERS + 1))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("username", is("user")))
+                .andDo(print());
+
+        verify(userService, times(1)).findUserById(Mockito.anyLong());
+    }
 
     // Delete user: success
     @Test
