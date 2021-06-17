6a8cc81e03f7966de75e9d0687f840cc25fa9b81
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-06-17 18:26:29 +0500

    Replace User with CustomPrincipal

--- src/main/java/ru/example/todo/service/TodoTaskService.java
+++ src/main/java/ru/example/todo/service/TodoTaskService.java
@@ -1,25 +1,28 @@
 package ru.example.todo.service;
 
+import ru.example.todo.domain.CustomPrincipal;
 import ru.example.todo.dto.TodoTaskDto;
 import ru.example.todo.entity.TodoTask;
 import ru.example.todo.entity.User;
-import ru.example.todo.enums.filters.FilterByDate;
 import ru.example.todo.enums.filters.FilterByBoolean;
+import ru.example.todo.enums.filters.FilterByDate;
 
 import java.util.List;
 import java.util.Set;
 
 public interface TodoTaskService {
 
-    List<TodoTask> findTasks(User user, Integer pageNo, Integer pageSize, FilterByDate date, String sort);
+    List<TodoTask> findTasks(CustomPrincipal principal, Integer pageNo,
+                             Integer pageSize, FilterByDate date, String sort);
 
-    TodoTask findTaskById(User user, Long taskId);
+    TodoTask findTaskById(CustomPrincipal principal, Long taskId);
 
-    void deleteTaskById(User user, Long taskId);
+    void deleteTaskById(CustomPrincipal principal, Long taskId);
 
     void createTask(User user, TodoTask task);
 
-    void updateTask(User user, Long sectionId, TodoTaskDto task, FilterByBoolean completed, FilterByBoolean starred);
+    void updateTask(CustomPrincipal principal, Long sectionId,
+                    TodoTaskDto task, FilterByBoolean completed, FilterByBoolean starred);
 
     List<TodoTask> findTasksByIds(Set<Long> taskIds, Long userId);
 
