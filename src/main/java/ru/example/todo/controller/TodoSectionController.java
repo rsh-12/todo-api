63f26e088e3605ebb7f32a359844e8a7141912d8
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-06-17 18:23:28 +0500

    Update some methods, replace UserDetailsImpls

--- src/main/java/ru/example/todo/controller/TodoSectionController.java
+++ src/main/java/ru/example/todo/controller/TodoSectionController.java
@@ -17,12 +17,14 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import ru.example.todo.controller.assembler.TodoSectionModelAssembler;
 import ru.example.todo.controller.wrapper.TaskIdsWrapper;
+import ru.example.todo.domain.CustomPrincipal;
 import ru.example.todo.dto.TodoSectionDto;
 import ru.example.todo.entity.TodoSection;
+import ru.example.todo.entity.User;
 import ru.example.todo.enums.filters.FilterByOperation;
 import ru.example.todo.facade.TasksFacade;
-import ru.example.todo.security.UserDetailsImpl;
 import ru.example.todo.service.TodoSectionService;
+import ru.example.todo.service.UserService;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -37,14 +39,16 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
 public class TodoSectionController {
 
+    private final UserService userService;
     private final TodoSectionService todoSectionService;
     private final TasksFacade tasksFacade;
     private final TodoSectionModelAssembler assembler;
     private final ModelMapper mapper;
 
     @Autowired
-    public TodoSectionController(TodoSectionService todoSectionService, TasksFacade tasksFacade,
+    public TodoSectionController(UserService userService, TodoSectionService todoSectionService, TasksFacade tasksFacade,
                                  TodoSectionModelAssembler assembler, ModelMapper mapper) {
+        this.userService = userService;
         this.todoSectionService = todoSectionService;
         this.tasksFacade = tasksFacade;
         this.assembler = assembler;
@@ -54,65 +58,66 @@ public class TodoSectionController {
     // get all sections
     @ApiOperation(value = "List todo sections", notes = "List all todo sections")
     @GetMapping(produces = "application/json")
-    public CollectionModel<EntityModel<TodoSection>> getSections(@AuthenticationPrincipal UserDetailsImpl uds) {
+    public CollectionModel<EntityModel<TodoSection>> getSections(@AuthenticationPrincipal CustomPrincipal principal) {
 
-        List<EntityModel<TodoSection>> sections = todoSectionService.findSectionDtoList(uds.getUser())
+        List<EntityModel<TodoSection>> sections = todoSectionService.findSectionDtoList(principal)
                 .stream()
                 .map(sectionDto -> mapper.map(sectionDto, TodoSection.class))
                 .map(assembler::toModel)
                 .collect(Collectors.toList());
 
         return CollectionModel.of(sections,
-                linkTo(methodOn(TodoSectionController.class).getSections(uds)).withSelfRel());
+                linkTo(methodOn(TodoSectionController.class).getSections(principal)).withSelfRel());
     }
 
     // get custom section by id
     @ApiOperation(value = "Find section", notes = "Find the Section by ID")
     @GetMapping(value = "/{id}", produces = "application/json")
-    public EntityModel<TodoSection> getSection(@AuthenticationPrincipal UserDetailsImpl uds,
+    public EntityModel<TodoSection> getSection(@AuthenticationPrincipal CustomPrincipal principal,
                                                @PathVariable("id") Long sectonId) {
 
-        return assembler.toModel(todoSectionService.findSectionById(uds.getUser(), sectonId));
+        return assembler.toModel(todoSectionService.findSectionById(principal, sectonId));
     }
 
     // delete section by id
     @ApiOperation(value = "Remove section", notes = "It permits to remove a section")
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteSection(@AuthenticationPrincipal UserDetailsImpl uds,
+    public ResponseEntity<String> deleteSection(@AuthenticationPrincipal CustomPrincipal principal,
                                                 @PathVariable("id") Long sectionId) {
-        todoSectionService.deleteSectionById(uds.getUser(), sectionId);
+        todoSectionService.deleteSectionById(principal, sectionId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     // create new section
     @ApiOperation(value = "Create section", notes = "It permits to create a new section")
     @PostMapping(consumes = "application/json")
-    public ResponseEntity<String> createSection(@AuthenticationPrincipal UserDetailsImpl uds,
+    public ResponseEntity<String> createSection(@AuthenticationPrincipal CustomPrincipal principal,
                                                 @Valid @RequestBody TodoSectionDto sectionDto) {
-        todoSectionService.createSection(uds.getUser(), sectionDto);
+        User user = userService.findUserByUsername(principal.getName());
+        todoSectionService.createSection(user, sectionDto);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     // update section title by id
     @ApiOperation(value = "Update section", notes = "It permits to update a section")
     @PutMapping(value = "/{id}", consumes = "application/json")
-    public ResponseEntity<String> updateSection(@AuthenticationPrincipal UserDetailsImpl uds,
+    public ResponseEntity<String> updateSection(@AuthenticationPrincipal CustomPrincipal principal,
                                                 @PathVariable("id") Long sectionId,
                                                 @Valid @RequestBody TodoSectionDto sectionDto) {
 
-        todoSectionService.updateSection(uds.getUser(), sectionId, sectionDto);
+        todoSectionService.updateSection(principal, sectionId, sectionDto);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     // add tasks to the list
     @ApiOperation(value = "Add tasks to section", notes = "It permits to add tasks to section")
     @PostMapping(value = "/{id}/tasks", consumes = "application/json")
-    public ResponseEntity<String> addOrRemoveTasks(@AuthenticationPrincipal UserDetailsImpl uds,
+    public ResponseEntity<String> addOrRemoveTasks(@AuthenticationPrincipal CustomPrincipal principal,
                                                    @PathVariable("id") Long sectionId,
                                                    @RequestBody TaskIdsWrapper wrapper,
                                                    @RequestParam(value = "do") FilterByOperation flag) {
 
-        tasksFacade.addTasksToOrRemoveFromSection(uds.getId(), sectionId, wrapper.tasks, flag);
+        tasksFacade.addTasksToOrRemoveFromSection(principal.getId(), sectionId, wrapper.tasks, flag);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
