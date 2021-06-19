cc8ead692fd2cd05a52276d11f0d58ba3131a206
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-06-19 13:51:04 +0500

    Remove createdAtm updatedAt fields

--- src/main/java/ru/example/todo/dto/TodoSectionDto.java
+++ src/main/java/ru/example/todo/dto/TodoSectionDto.java
@@ -7,7 +7,6 @@ package ru.example.todo.dto;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
-import java.util.Date;
 
 public class TodoSectionDto {
 
@@ -16,21 +15,12 @@ public class TodoSectionDto {
     @Size(min = 3, max = 50, message = "Size must be between 3 and 50")
     private String title;
 
-    private Date createdAt;
-
-    private Date updatedAt;
 
     public TodoSectionDto() {
     }
 
     public TodoSectionDto(String title) {
         this.title = title;
-    }
-
-    public TodoSectionDto(String title, Date createdAt, Date updatedAt) {
-        this.title = title;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
     }
 
     public String getTitle() {
@@ -41,28 +31,10 @@ public class TodoSectionDto {
         this.title = title;
     }
 
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Date getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(Date updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
     @Override
     public String toString() {
         return "TodoSectionDto{" +
                 "title='" + title + '\'' +
-                ", createdAt=" + createdAt +
-                ", updatedAt=" + updatedAt +
                 '}';
     }
 
