f5468241e7d126b855f700d6575bd7d3564c99a7
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-06-19 14:20:55 +0500

    Add custom lazy query method

--- src/main/java/ru/example/todo/repository/TodoSectionRepository.java
+++ src/main/java/ru/example/todo/repository/TodoSectionRepository.java
@@ -3,6 +3,7 @@ package ru.example.todo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import ru.example.todo.domain.TodoSectionProjection;
 import ru.example.todo.dto.TodoSectionDto;
 import ru.example.todo.entity.TodoSection;
 import ru.example.todo.entity.User;
@@ -11,6 +12,10 @@ import java.util.List;
 import java.util.Optional;
 
 public interface TodoSectionRepository extends JpaRepository<TodoSection, Long> {
+
+    @Query("select new ru.example.todo.domain.TodoSectionProjection(s.id, s.title, s.updatedAt, s.createdAt) " +
+            "from TodoSection s where s.user.id = :userId")
+    List<TodoSectionProjection> findAllByUserIdProjection(@Param("userId") Long id);
 
     // todo: fix eager loading
     List<TodoSection> findAllByUserId(@Param("userId") Long id);
