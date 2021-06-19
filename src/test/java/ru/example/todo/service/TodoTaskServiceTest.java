5257b2df1d34f7c4cc25b786db33b1d9c1286f1d
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-06-19 12:10:04 +0500

    Add findTaskById(ok) test method

--- src/test/java/ru/example/todo/service/TodoTaskServiceTest.java
+++ src/test/java/ru/example/todo/service/TodoTaskServiceTest.java
@@ -4,9 +4,16 @@ package ru.example.todo.service;
  * Time: 10:21 AM
  * */
 
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import ru.example.todo.entity.TodoTask;
 import ru.example.todo.repository.TodoTaskRepository;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
 
 public class TodoTaskServiceTest extends AbstractServiceTestClass {
 
@@ -17,7 +24,21 @@ public class TodoTaskServiceTest extends AbstractServiceTestClass {
     private TodoTaskRepository taskRepository;
 
     //  findTasks
+
     //  findTaskById
+    @Test
+    public void findTaskById_ShouldReturnTask() {
+        given(taskRepository.findByIdAndUserId(anyLong(), anyLong()))
+                .willReturn(java.util.Optional.of(new TodoTask("Title")));
+        TodoTask task = taskService.findTaskById(1L, 1L);
+
+        assertNotNull(task);
+        assertEquals("Title", task.getTitle());
+
+        verify(taskRepository).findByIdAndUserId(anyLong(), anyLong());
+        verifyNoMoreInteractions(taskRepository);
+    }
+
     //  deleteTaskById
     //  createTask
     //  updateTask
