47835eb5a992c03a0906b9888ca7ff95818d8e9f
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-06-19 12:32:49 +0500

    Add findTasksByIds test method

--- src/test/java/ru/example/todo/service/TodoTaskServiceTest.java
+++ src/test/java/ru/example/todo/service/TodoTaskServiceTest.java
@@ -12,8 +12,12 @@ import ru.example.todo.entity.TodoTask;
 import ru.example.todo.exception.CustomException;
 import ru.example.todo.repository.TodoTaskRepository;
 
+import java.util.List;
+import java.util.Set;
+
 import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
@@ -52,6 +56,19 @@ public class TodoTaskServiceTest extends AbstractServiceTestClass {
     //  deleteTaskById
     //  createTask
     //  updateTask
+
     //  findTasksByIds
+    @Test
+    public void findTasksByIds_ShouldReturnListOfTasks() {
+        given(taskRepository.findAllByIdInAndUserId(anySet(), anyLong()))
+                .willReturn(List.of(new TodoTask("Task1"), new TodoTask("Task2")));
+        List<TodoTask> tasks = taskService.findTasksByIds(Set.of(1L, 2L), 1L);
+
+        assertNotNull(tasks);
+        assertEquals(2, tasks.size());
+
+        assertTrue(tasks.get(0).getTitle().startsWith("Task"));
+        assertTrue(tasks.get(1).getTitle().startsWith("Task"));
+    }
 
 }
