06d8a6a7f786f00def850fa5880403e25bf63db3
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-06-19 12:24:01 +0500

    Add findTaskById(fail) test method

--- src/test/java/ru/example/todo/service/TodoTaskServiceTest.java
+++ src/test/java/ru/example/todo/service/TodoTaskServiceTest.java
@@ -7,13 +7,16 @@ package ru.example.todo.service;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import ru.example.todo.entity.TodoTask;
+import ru.example.todo.exception.CustomException;
 import ru.example.todo.repository.TodoTaskRepository;
 
 import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 
 public class TodoTaskServiceTest extends AbstractServiceTestClass {
 
@@ -37,6 +40,13 @@ public class TodoTaskServiceTest extends AbstractServiceTestClass {
 
         verify(taskRepository).findByIdAndUserId(anyLong(), anyLong());
         verifyNoMoreInteractions(taskRepository);
+    }
+
+    @Test
+    public void findTaskById_ShouldThrowException() {
+        given(taskRepository.findByIdAndUserId(anyLong(), anyLong()))
+                .willThrow(new CustomException("Not Found", "Task not found", HttpStatus.NOT_FOUND));
+        assertThrows(CustomException.class, () -> taskService.findTaskById(1L, 1L));
     }
 
     //  deleteTaskById
