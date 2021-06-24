888c566913b0515bc91ce89be3aa3dd9c3105157
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-06-24 20:12:58 +0500

    Add some comments, getUsername test method

--- src/test/java/ru/example/todo/service/JwtTokenServiceTest.java
+++ src/test/java/ru/example/todo/service/JwtTokenServiceTest.java
@@ -12,6 +12,7 @@ import ru.example.todo.enums.Role;
 import ru.example.todo.exception.CustomException;
 import ru.example.todo.service.impl.JwtTokenServiceImpl;
 
+import java.util.Collections;
 import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
@@ -49,5 +50,26 @@ public class JwtTokenServiceTest extends AbstractServiceTestClass {
         assertEquals(username, refreshToken.getUsername());
         System.out.println("refreshToken = " + refreshToken);
     }
+
+    // resolveAccessToken
+    // isAccessTokenValid
+    // findRefreshToken
+    // removeRefreshTokenById
+    // hasRefreshTokenExpired
+
+    // getUsername
+    @Test
+    public void getUsername_ShouldReturnUsername() {
+        String accessToken = jwtTokenService
+                .buildAccessToken(1L, "user@mail.com", Collections.singleton(Role.USER));
+
+        assertNotNull(accessToken);
+
+        String username = jwtTokenService.getUsername(accessToken);
+        assertEquals("user@mail.com", username);
+    }
+
+    // getUserRoles
+    // getAuthentication
 
 }
