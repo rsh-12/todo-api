7aa386f7640b5c43f0fe3e9a15fc2cfaea1c3121
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-06-30 09:10:01 +0500

    Add isAccessToken(exception) valid test method

--- src/test/java/ru/example/todo/service/JwtTokenServiceTest.java
+++ src/test/java/ru/example/todo/service/JwtTokenServiceTest.java
@@ -4,15 +4,20 @@ package ru.example.todo.service;
  * Time: 9:01 AM
  * */
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import ru.example.todo.config.properties.TokenProperties;
 import ru.example.todo.enums.Role;
 import ru.example.todo.exception.CustomException;
 import ru.example.todo.service.impl.JwtTokenServiceImpl;
 
 import javax.servlet.http.HttpServletRequest;
+import java.nio.charset.StandardCharsets;
 import java.util.Collections;
+import java.util.Date;
+import java.util.Map;
 import java.util.Set;
 
 import static org.junit.Assert.*;
@@ -24,6 +29,8 @@ public class JwtTokenServiceTest extends AbstractServiceTestClass {
     @Autowired
     private JwtTokenServiceImpl jwtTokenService;
 
+    @Autowired
+    private TokenProperties tokenProperties;
 
     // buildAccessToken
     @Test
@@ -52,6 +59,17 @@ public class JwtTokenServiceTest extends AbstractServiceTestClass {
         String accessToken = jwtTokenService.buildAccessToken(1L, Collections.singleton(Role.USER));
         boolean isAccessTokenValid = jwtTokenService.isAccessTokenValid(accessToken);
         assertTrue(isAccessTokenValid);
+    }
+
+    @Test
+    public void isAccessTokenValid_ShouldThrowCustomException() {
+        String accessToken = Jwts.builder()
+                .setClaims(Map.of("id", 1L))
+                .setExpiration(new Date(new Date().getTime() - 1000))
+                .signWith(Keys.hmacShaKeyFor(tokenProperties.getSecret().getBytes(StandardCharsets.UTF_8)))
+                .compact();
+
+        assertThrows(CustomException.class, () -> jwtTokenService.isAccessTokenValid(accessToken));
     }
 
     // getId
