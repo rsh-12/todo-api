d9cfbd2b27aea542770bd54ede178a022672806f
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-07-05 10:52:05 +0500

    Replace string with Map, UserDto with User

--- src/main/java/ru/example/todo/service/UserService.java
+++ src/main/java/ru/example/todo/service/UserService.java
@@ -1,23 +1,24 @@
 package ru.example.todo.service;
 
-import ru.example.todo.dto.UserDto;
 import ru.example.todo.entity.User;
 import ru.example.todo.exception.CustomException;
+
+import java.util.Map;
 
 public interface UserService {
 
     /**
      * Authenticates (not completed) the user and returns access and refresh tokens.
      *
-     * @param userDto contains username (email) and password.
+     * @param user contains username (email) and password.
      * @return access, refresh tokens, token type and expiration time in milliseconds.
      * @throws CustomException if the user by username not found in the database.
      */
-    String login(UserDto userDto, String ip);
+    Map<String, String> login(User user, String ip);
 
     String register(User user);
 
-    String generateNewTokens(String refreshToken, String ip);
+    Map<String, String> generateNewTokens(String refreshToken, String ip);
 
     void deleteUserById(Long userId);
 
