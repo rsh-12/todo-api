e468eb4a7728308e9715920b1798e2523300329c
rsh-12 <grinvichforum12@mail.ru>
2021-07-07 10:24:03 +0500

    Optimize imports

--- src/test/java/ru/example/todo/service/RefreshTokenServiceTest.java
+++ src/test/java/ru/example/todo/service/RefreshTokenServiceTest.java
@@ -15,15 +15,13 @@ import ru.example.todo.entity.RefreshToken;
 import ru.example.todo.repository.RefreshTokenRepository;
 import ru.example.todo.service.impl.RefreshTokenServiceImpl;
 
-import java.text.SimpleDateFormat;
 import java.time.Instant;
-import java.time.LocalDate;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.Optional;
 
 import static org.junit.Assert.*;
-import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
