80fb556ceeed2bdc0e54f1da6f040d73b61be321
rsh-12 <grinvichforum12@mail.ru>
2021-07-09 16:43:19 +0500

    Update javadocs

--- src/main/java/ru/example/todo/service/JwtTokenService.java
+++ src/main/java/ru/example/todo/service/JwtTokenService.java
@@ -7,15 +7,19 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.Set;
 
 /**
- * The interface contains methods for working with the JWT.
+ * This interface consists methods for working with the JWT token.
+ *
+ * @see ru.example.todo.service.impl.JwtTokenServiceImpl
  */
 public interface JwtTokenService {
 
     /**
-     * Builds an access JWT.
+     * Returns an access token as a string.
      *
-     * @param roles the roles for building access token.
-     * @return the access JWT
+     * @param userId the user id
+     * @param roles  the user roles
+     * @return the signed JWT as a string with user's roles and id in the body
+     * @see io.jsonwebtoken.Jwts
      */
     String buildAccessToken(Long userId, Set<Role> roles);
 
@@ -23,21 +27,50 @@ public interface JwtTokenService {
      * Gets the access token from header param "Authorization".
      *
      * @param request the http request
-     * @return the access token from request.
+     * @return the access token as a string from request, if the Authorization header
+     * is missing, returns an empty string
      */
     String resolveAccessToken(HttpServletRequest request);
 
     /**
-     * Validates the access token.
+     * Checks whether an access token has expired or not.
      *
-     * @param accessToken the access token.
-     * @return the boolean: <b>true</b> if the token is valid, <b>false</b> otherwise.
+     * @param accessToken the access token as a string
+     * @return the boolean
+     * @throws ru.example.todo.exception.CustomException if the access token has expired
      */
     boolean isAccessTokenValid(String accessToken);
 
+    /**
+     * Gets the user id from the access token body.
+     *
+     * @param accessToken the access token as a string
+     * @return the user id as a Long
+     */
     Long getId(String accessToken);
 
+    /**
+     * Gets user roles from the access token body.
+     *
+     * @param accessToken the access token as a string
+     * @return the user roles
+     */
     Set<Role> getUserRoles(String accessToken);
 
+    /**
+     * Retrieves the user data from the access token body:
+     * id and roles. Creates an instance with of the User class
+     * with the received data, then the UserDetailsImpl instance,
+     * passing the created User instance to the constructor.
+     * Returns an instance of the UsernamePasswordAuthenticationToken,
+     * passing a UserDetailsImpl instance, empty string and user roles
+     * to the constructor.
+     *
+     * @param accessToken the access token as a string
+     * @return the authentication
+     * @see ru.example.todo.entity.User
+     * @see ru.example.todo.security.UserDetailsImpl
+     * @see org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+     */
     Authentication getAuthentication(String accessToken);
 }
