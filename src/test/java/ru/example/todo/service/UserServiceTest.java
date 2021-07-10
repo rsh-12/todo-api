d9354aec99e24c7cefc11292ce2c9f22ce7123ac
rsh-12 <grinvichforum12@mail.ru>
2021-07-11 04:49:24 +0500

    Add login(ok) test method

---
+++ src/test/java/ru/example/todo/service/UserServiceTest.java
@@ -0,0 +1,86 @@
+package ru.example.todo.service;
+/*
+ * Date: 10.07.2021
+ * Time: 6:12 PM
+ * */
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import ru.example.todo.config.properties.TokenProperties;
+import ru.example.todo.entity.User;
+import ru.example.todo.repository.UserRepository;
+import ru.example.todo.security.UserDetailsImpl;
+import ru.example.todo.service.impl.UserServiceImpl;
+
+import java.util.Map;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserServiceTest {
+
+    @InjectMocks
+    private UserServiceImpl userService;
+
+    @Mock
+    private AuthenticationManager authManager;
+
+    @Mock
+    private JwtTokenService jwtTokenService;
+
+    @Mock
+    private RefreshTokenService refreshTokenService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private TokenProperties tokenProperties;
+
+    @Before
+    public void setUp() {
+        given(tokenProperties.getAccessTokenValidity()).willReturn(1_800_000L);
+        given(tokenProperties.getRefreshTokenValidity()).willReturn(86_400_000L);
+    }
+
+    // login
+    @Test
+    public void login_ShouldAuthUserAndReturnTokens() {
+        User user = mock(User.class);
+        Authentication auth = mock(Authentication.class);
+
+        given(authManager.authenticate(any(UsernamePasswordAuthenticationToken.class))).willReturn(auth);
+        given(auth.getPrincipal()).willReturn(new UserDetailsImpl(user));
+
+        given(jwtTokenService.buildAccessToken(anyLong(), anySet())).willReturn("accessToken");
+        given(refreshTokenService.createRefreshToken(anyLong(), anyString())).willReturn("refreshToken");
+
+        Map<String, String> response = userService.login(new User(), "");
+
+        assertEquals("accessToken", response.get("access_token"));
+        assertEquals("refreshToken", response.get("refresh_token"));
+        assertEquals("Bearer", response.get("token_type"));
+        assertEquals("1800000", response.get("access_token_expires"));
+        assertEquals("86400000", response.get("refresh_token_expires"));
+    }
+
+
+    // register
+    // generateNewTokens
+    // deleteUserById
+    // findUserById
+    // updatePassword
+    // existsByUsername
+
+}
