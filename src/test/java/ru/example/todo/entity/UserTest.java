e5dd16322e812c931746cc84bffea63dad098e75
rsh-12 <grinvichforum12@mail.ru>
2021-07-23 11:14:18 +0500

    Remove unnecessary User instances

--- src/test/java/ru/example/todo/entity/UserTest.java
+++ src/test/java/ru/example/todo/entity/UserTest.java
@@ -35,41 +35,36 @@ public class UserTest {
     @Test
     public void setRoles_ShouldContainAllRoles() {
         Set<Role> roles = Set.of(Role.USER, Role.ADMIN);
-        User user = new User();
         user.setRoles(roles);
         assertTrue(user.getRoles().containsAll(roles));
     }
 
     @Test
     public void clearRoles_ShouldClearRoles() {
-        User user = new User();
         assertTrue(user.getRoles().contains(Role.USER));
-
         user.clearRoles();
+
         assertFalse(user.getRoles().stream()
                 .anyMatch(role -> role.equals(Role.USER) || role.equals(Role.ADMIN)));
     }
 
     @Test
     public void equals_ShouldBeEqual() {
-        User user1 = new User(1L, Collections.singleton(Role.USER));
         User user2 = new User(1L, Collections.singleton(Role.USER));
-        assertEquals(user1, user2);
+        assertEquals(user, user2);
     }
 
     @Test
     public void equals_ShouldNotBeEqual() {
-        User user1 = new User(1L, Collections.singleton(Role.USER));
         User user2 = new User(1L, Collections.singleton(Role.ADMIN));
-        assertNotEquals(user1, user2);
+        assertNotEquals(user, user2);
     }
 
     @Test
     public void equals_Symmetric_ShouldBeEqual() {
-        User user1 = new User(1L, Collections.singleton(Role.USER));
         User user2 = new User(1L, Collections.singleton(Role.USER));
-
-        assertTrue(user1.equals(user2) && user2.equals(user1));
-        assertEquals(user1.hashCode(), user2.hashCode());
+        assertTrue(user.equals(user2) && user2.equals(user));
+        assertEquals(user.hashCode(), user2.hashCode());
     }
+
 }
