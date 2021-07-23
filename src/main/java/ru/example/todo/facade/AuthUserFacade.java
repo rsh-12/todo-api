8ea51ae91ae9fdf3d5a45253865d45c3fa2014cd
rsh-12 <grinvichforum12@mail.ru>
2021-07-23 18:32:59 +0500

    Add AuthUserFacade for getting current logged user

---
+++ src/main/java/ru/example/todo/facade/AuthUserFacade.java
@@ -0,0 +1,13 @@
+package ru.example.todo.facade;
+/*
+ * Date: 23.07.2021
+ * Time: 5:11 PM
+ * */
+
+import ru.example.todo.entity.User;
+
+public interface AuthUserFacade {
+
+    User getLoggedUser();
+
+}
---
+++ src/main/java/ru/example/todo/facade/impl/AuthUserFacadeImpl.java
@@ -0,0 +1,24 @@
+package ru.example.todo.facade.impl;
+/*
+ * Date: 23.07.2021
+ * Time: 5:13 PM
+ * */
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import ru.example.todo.entity.User;
+import ru.example.todo.facade.AuthUserFacade;
+import ru.example.todo.security.UserDetailsImpl;
+
+@Component
+public class AuthUserFacadeImpl implements AuthUserFacade {
+
+    @Override
+    public User getLoggedUser() {
+        UserDetailsImpl principal = (UserDetailsImpl) SecurityContextHolder.getContext()
+                .getAuthentication()
+                .getPrincipal();
+
+        return principal.getUser();
+    }
+}
