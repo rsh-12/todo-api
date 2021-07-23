aaa5efaa5193a504c12c672b2f66f5eebc203aec
rsh-12 <grinvichforum12@mail.ru>
2021-07-23 21:37:14 +0500

    Optimize imports

--- src/test/java/ru/example/todo/entity/UserTest.java
+++ src/test/java/ru/example/todo/entity/UserTest.java
@@ -12,7 +12,10 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.Set;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class UserTest {
 
