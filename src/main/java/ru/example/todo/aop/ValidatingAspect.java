b4b94939532987105f677046f86bf6b33532b0af
rsh-12 <grinvichforum12@mail.ru>
2021-07-24 08:54:54 +0500

    Remove section validation aop

--- src/main/java/ru/example/todo/aop/ValidatingAspect.java
+++ src/main/java/ru/example/todo/aop/ValidatingAspect.java
@@ -8,12 +8,10 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
-import ru.example.todo.entity.TodoSection;
 import ru.example.todo.entity.TodoTask;
 import ru.example.todo.entity.User;
 import ru.example.todo.enums.Role;
 import ru.example.todo.exception.CustomException;
-import ru.example.todo.repository.TodoSectionRepository;
 import ru.example.todo.repository.TodoTaskRepository;
 
 @Aspect
@@ -21,41 +19,15 @@ import ru.example.todo.repository.TodoTaskRepository;
 public class ValidatingAspect {
 
     private final TodoTaskRepository taskRepository;
-    private final TodoSectionRepository sectionRepository;
 
-    public ValidatingAspect(TodoTaskRepository taskRepository, TodoSectionRepository sectionRepository) {
+    public ValidatingAspect(TodoTaskRepository taskRepository) {
         this.taskRepository = taskRepository;
-        this.sectionRepository = sectionRepository;
     }
 
     @Pointcut("execution(* ru.example.todo.service.impl.TodoTaskServiceImpl.deleteTaskById(..))")
     private void deleteTask() {
     }
 
-    @Pointcut("execution(* ru.example.todo.service.impl.TodoSectionServiceImpl.deleteSectionById(..))")
-    private void deleteSection() {
-    }
-
-    @Pointcut("execution(* ru.example.todo.service.impl.TodoSectionServiceImpl.updateSection(..))")
-    private void updateSection() {
-    }
-
-    @Before(value = "updateSection() && args(principal, sectionId, todoSection))",
-            argNames = "principal,sectionId,todoSection")
-    public void validateSectionUpdating(User principal, Long sectionId, TodoSection todoSection) {
-        validateSectionOps(principal, sectionId);
-    }
-
-    @Before(value = "deleteSection() && args(principal, sectionId))", argNames = "principal,sectionId")
-    public void validateSectionDeleting(User principal, Long sectionId) {
-        validateSectionOps(principal, sectionId);
-    }
-
-    private void validateSectionOps(User principal, Long sectionId) {
-        TodoSection section = sectionRepository.findById(sectionId)
-                .orElseThrow(() -> CustomException.notFound("Section not found: id=" + sectionId));
-        equalsOrHasRoleAdmin(principal, section.getUser());
-    }
 
     @Before(value = "deleteTask() && args(principal, taskId)", argNames = "principal,taskId")
     public void validateTaskDeleting(User principal, Long taskId) {
