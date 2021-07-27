f687983943a22ab66cc2eef9cd83bbb70c2e4c97
rsh-12 <grinvichforum12@mail.ru>
2021-07-27 08:57:24 +0500

    Remove unnecessary query methods

--- src/main/java/ru/example/todo/repository/TodoSectionRepository.java
+++ src/main/java/ru/example/todo/repository/TodoSectionRepository.java
@@ -4,9 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import ru.example.todo.domain.TodoSectionProjection;
-import ru.example.todo.dto.TodoSectionDto;
 import ru.example.todo.entity.TodoSection;
-import ru.example.todo.entity.User;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,11 +14,6 @@ public interface TodoSectionRepository extends JpaRepository<TodoSection, Long> 
     @Query("select new ru.example.todo.domain.TodoSectionProjection(s.id, s.title, s.updatedAt, s.createdAt) " +
             "from TodoSection s where s.user.id = :userId")
     List<TodoSectionProjection> findAllByUserIdProjection(@Param("userId") Long id);
-
-    // todo: fix eager loading
-    List<TodoSection> findAllByUserId(@Param("userId") Long id);
-
-    List<TodoSection> findAllByUser(User user);
 
     Optional<TodoSection> findByUserIdAndId(Long userId, Long sectionId);
 
