7083c1cb584c5cb6683c85dd7c793307d2e65422
rsh-12 <grinvichforum12@mail.ru>
2021-07-29 10:28:30 +0500

    Remove ModelMapper, optimize imports

--- src/main/java/ru/example/todoapp/controller/AuthController.java
+++ src/main/java/ru/example/todoapp/controller/AuthController.java
@@ -6,7 +6,6 @@ package ru.example.todoapp.controller;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import io.swagger.annotations.Api;
-import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,8 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.example.todoapp.controller.request.CredentialsRequest;
-import ru.example.todoapp.dto.UserDto;
-import ru.example.todoapp.entity.User;
 import ru.example.todoapp.facade.PasswordFacade;
 import ru.example.todoapp.messaging.MessagingClient;
 import ru.example.todoapp.messaging.request.EmailRequest;
@@ -34,14 +31,11 @@ public class AuthController {
     private final UserService userService;
     private final PasswordFacade passwordFacade;
     private final MessagingClient messagingService;
-    private final ModelMapper modelMapper;
 
-    public AuthController(UserService userService, PasswordFacade passwordFacade,
-                          MessagingClient messagingService, ModelMapper modelMapper) {
+    public AuthController(UserService userService, PasswordFacade passwordFacade, MessagingClient messagingService) {
         this.userService = userService;
         this.passwordFacade = passwordFacade;
         this.messagingService = messagingService;
-        this.modelMapper = modelMapper;
     }
 
     @PostMapping(value = "/login", produces = "application/json")
@@ -52,8 +46,8 @@ public class AuthController {
     }
 
     @PostMapping(value = "/register", produces = "application/json")
-    public ResponseEntity<String> register(@Valid @RequestBody UserDto userDto) {
-        userService.register(modelMapper.map(userDto, User.class));
+    public ResponseEntity<String> register(@Valid @RequestBody CredentialsRequest credentials) {
+        userService.register(credentials);
         return ResponseEntity.ok().build();
     }
 
