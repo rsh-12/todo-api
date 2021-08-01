7c671003234cde53185c9412ab59b8dea112c862
rsh-12 <grinvichforum12@mail.ru>
2021-08-01 20:20:10 +0500

    Add JsonAutoDetect annotation

--- src/main/java/ru/example/todoapp/dto/UserDto.java
+++ src/main/java/ru/example/todoapp/dto/UserDto.java
@@ -1,7 +1,10 @@
 package ru.example.todoapp.dto;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+
 import java.time.LocalDateTime;
 
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public record UserDto(String username, LocalDateTime createdAt) {
 
 }
