3376604b30e6cd5a8897e24697a371dfbb1f1386
rsh-12 <grinvichforum12@mail.ru>
2021-08-01 09:52:16 +0500

    Remove GrantedAuthority interface

--- src/main/java/ru/example/todoapp/enums/Role.java
+++ src/main/java/ru/example/todoapp/enums/Role.java
@@ -1,13 +1,7 @@
 package ru.example.todoapp.enums;
 
-import org.springframework.security.core.GrantedAuthority;
-
-public enum Role implements GrantedAuthority {
+public enum Role {
 
     USER, ADMIN;
 
-    @Override
-    public String getAuthority() {
-        return name();
-    }
 }
