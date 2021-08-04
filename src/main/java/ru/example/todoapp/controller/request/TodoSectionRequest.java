e2eced748be1c50c3d924e835dcadf1a3300b548
rsh-12 <grinvichforum12@mail.ru>
2021-08-04 09:41:19 +0500

    Replace record with class

--- src/main/java/ru/example/todoapp/controller/request/TodoSectionRequest.java
+++ src/main/java/ru/example/todoapp/controller/request/TodoSectionRequest.java
@@ -1,13 +1,32 @@
 package ru.example.todoapp.controller.request;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
-@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
-public record TodoSectionRequest(
-        @NotBlank
-        @Size(min = 3, max = 50, message = "Size must be between 3 and 50")
-        String title) {
+public class TodoSectionRequest {
+
+    @NotBlank
+    @Size(min = 3, max = 50, message = "Size must be between 3 and 50")
+    private String title;
+
+    public TodoSectionRequest() {
+    }
+
+    public TodoSectionRequest(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return title;
+    }
+
 }
