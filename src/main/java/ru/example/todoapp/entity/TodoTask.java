d03f205e94414b846b20a52d96dde8316fc8f8f3
rsh-12 <grinvichforum12@mail.ru>
2021-08-04 10:28:28 +0500

    Replace Date with LocalDateTime, make minor fixes

--- src/main/java/ru/example/todoapp/entity/TodoTask.java
+++ src/main/java/ru/example/todoapp/entity/TodoTask.java
@@ -10,10 +10,19 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.hateoas.server.core.Relation;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
-import java.util.Date;
+import java.time.LocalDateTime;
 import java.util.Objects;
 
 @Entity
@@ -40,11 +49,11 @@ public class TodoTask {
 
     @JsonFormat(timezone = "Asia/Yekaterinburg")
     @CreationTimestamp
-    private Date createdAt;
+    private LocalDateTime createdAt;
 
     @JsonFormat(timezone = "Asia/Yekaterinburg")
     @UpdateTimestamp
-    private Date updatedAt;
+    private LocalDateTime updatedAt;
 
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @ManyToOne(fetch = FetchType.LAZY,
@@ -53,7 +62,6 @@ public class TodoTask {
     @JoinColumn(name = "list_id")
     private TodoSection todoSection;
 
-    // @NotNull
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", referencedColumnName = "id")
@@ -107,19 +115,19 @@ public class TodoTask {
         this.completionDate = Objects.requireNonNullElseGet(completionDate, LocalDate::now);
     }
 
-    public Date getCreatedAt() {
+    public LocalDateTime getCreatedAt() {
         return createdAt;
     }
 
-    public void setCreatedAt(Date createdAt) {
+    public void setCreatedAt(LocalDateTime createdAt) {
         this.createdAt = createdAt;
     }
 
-    public Date getUpdatedAt() {
+    public LocalDateTime getUpdatedAt() {
         return updatedAt;
     }
 
-    public void setUpdatedAt(Date updatedAt) {
+    public void setUpdatedAt(LocalDateTime updatedAt) {
         this.updatedAt = updatedAt;
     }
 
