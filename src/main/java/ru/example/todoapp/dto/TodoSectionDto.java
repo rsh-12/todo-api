08ba8b5555cdbecf59d615a5d622cd0bf578eff8
rsh-12 <grinvichforum12@mail.ru>
2021-08-05 07:15:17 +0500

    Add JsonAutoDetect, Relation

--- src/main/java/ru/example/todoapp/dto/TodoSectionDto.java
+++ src/main/java/ru/example/todoapp/dto/TodoSectionDto.java
@@ -4,8 +4,13 @@ package ru.example.todoapp.dto;
  * Time: 5:46 PM
  * */
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import org.springframework.hateoas.server.core.Relation;
+
 import java.time.LocalDateTime;
 
-public record TodoSectionDto(String title, LocalDateTime updatedAt, LocalDateTime createdAt) {
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+@Relation(value = "section", collectionRelation = "sections")
+public record TodoSectionDto(Long id, String title, LocalDateTime updatedAt, LocalDateTime createdAt) {
 
 }
