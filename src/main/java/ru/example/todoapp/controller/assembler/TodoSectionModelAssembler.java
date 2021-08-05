47bcedf43b8aaa758360df652b6be35a239be2f7
rsh-12 <grinvichforum12@mail.ru>
2021-08-05 07:15:27 +0500

    Update TodoSectionModelAssembler

--- src/main/java/ru/example/todoapp/controller/assembler/TodoSectionModelAssembler.java
+++ src/main/java/ru/example/todoapp/controller/assembler/TodoSectionModelAssembler.java
@@ -8,18 +8,19 @@ import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
 import ru.example.todoapp.controller.TodoSectionController;
-import ru.example.todoapp.entity.TodoSection;
+import ru.example.todoapp.dto.TodoSectionDto;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Component
-public class TodoSectionModelAssembler implements RepresentationModelAssembler<TodoSection, EntityModel<TodoSection>> {
+public class TodoSectionModelAssembler implements
+        RepresentationModelAssembler<TodoSectionDto, EntityModel<TodoSectionDto>> {
 
     @Override
-    public EntityModel<TodoSection> toModel(TodoSection section) {
+    public EntityModel<TodoSectionDto> toModel(TodoSectionDto section) {
         return EntityModel.of(section,
-                linkTo(methodOn(TodoSectionController.class).getSection(section.getId())).withSelfRel(),
+                linkTo(methodOn(TodoSectionController.class).getSection(section.id())).withSelfRel(),
                 linkTo(methodOn(TodoSectionController.class).getSections()).withRel("sections"));
     }
 }
