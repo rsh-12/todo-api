d149733f44d055bf56857e35ad9f99955e28f7ad
rsh-12 <grinvichforum12@mail.ru>
2021-08-16 09:06:03 +0500

    Optimize imports

--- src/test/java/ru/example/todoapp/service/JwtTokenServiceTest.java
+++ src/test/java/ru/example/todoapp/service/JwtTokenServiceTest.java
@@ -25,7 +25,11 @@ import java.util.Date;
 import java.util.Map;
 import java.util.Set;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 
