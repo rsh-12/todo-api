950a4bc718f16ef3da7cea49d007c179669324b9
rsh-12 <grinvichforum12@mail.ru>
2021-08-18 18:56:12 +0500

    Update some query methods

--- src/main/java/ru/example/todoapp/repository/TodoTaskRepository.java
+++ src/main/java/ru/example/todoapp/repository/TodoTaskRepository.java
@@ -1,5 +1,6 @@
 package ru.example.todoapp.repository;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.example.todoapp.entity.TodoTask;
@@ -11,13 +12,13 @@ import java.util.Set;
 
 public interface TodoTaskRepository extends JpaRepository<TodoTask, Long> {
 
-    List<TodoTask> findAllByCompletionDateEqualsAndUserId(LocalDate date, Pageable page, Long userId);
+    Page<TodoTask> findAllByCompletionDateEqualsAndUserId(LocalDate date, Long userId, Pageable pageable);
 
-    List<TodoTask> findAllByCompletionDateBeforeAndUserId(LocalDate date, Pageable page, Long userId);
+    Page<TodoTask> findAllByCompletionDateBeforeAndUserId(LocalDate date, Long userId, Pageable pageable);
 
     List<TodoTask> findAllByIdInAndUserId(Set<Long> taskIds, Long userId);
 
-    List<TodoTask> findAllByUserId(Long userId, Pageable pageable);
+    Page<TodoTask> findAllByUserId(Long userId, Pageable pageable);
 
     Optional<TodoTask> findByIdAndUserId(Long taskId, Long userId);
 
