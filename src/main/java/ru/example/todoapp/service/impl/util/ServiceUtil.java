3709c61e1f99d81273ec1aa4383919e02b743259
rsh-12 <grinvichforum12@mail.ru>
2021-08-18 18:57:38 +0500

    Remove unnecessary util methods

--- src/main/java/ru/example/todoapp/service/impl/util/ServiceUtil.java
+++ src/main/java/ru/example/todoapp/service/impl/util/ServiceUtil.java
@@ -4,23 +4,12 @@ package ru.example.todoapp.service.impl.util;
  * Time: 12:33 AM
  * */
 
-import org.springframework.data.domain.Sort;
 import ru.example.todoapp.dto.UserDto;
 import ru.example.todoapp.entity.User;
 import ru.example.todoapp.enums.Role;
 import ru.example.todoapp.exception.CustomException;
 
 public record ServiceUtil() {
-
-    public static Sort.Direction getSortDirection(String sort) {
-        if (sort.contains(",asc")) return Sort.Direction.ASC;
-        return Sort.Direction.DESC;
-    }
-
-    public static String getSortAsString(String sort) {
-        if (sort.contains(",")) return sort.split(",")[0];
-        return sort;
-    }
 
     public static void validateUser(User principal, User owner) {
         boolean isValid = (owner != null && owner.equals(principal)) || principal.getRoles().contains(Role.ADMIN);
