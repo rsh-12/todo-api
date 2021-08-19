478df6045c6ce23b08d8bb8b79a426d13b8c7f55
rsh-12 <grinvichforum12@mail.ru>
2021-08-19 19:45:49 +0500

    Rewrite TaskBuilder

--- src/main/java/ru/example/todoapp/dsl/TaskBuilder.java
+++ src/main/java/ru/example/todoapp/dsl/TaskBuilder.java
@@ -8,33 +8,42 @@ import ru.example.todoapp.entity.TodoTask;
 import ru.example.todoapp.entity.User;
 
 import java.time.LocalDate;
-import java.util.function.Consumer;
 
 public class TaskBuilder {
 
     private final TodoTask task = new TodoTask();
 
-    public static TodoTask task(Consumer<TaskBuilder> consumer) {
-        TaskBuilder builder = new TaskBuilder();
-        consumer.accept(builder);
-
-        return builder.task;
-    }
-
-    public void title(String title) {
+    private TaskBuilder(String title, LocalDate completionDate) {
         task.setTitle(title);
+        task.setCompletionDate(completionDate);
     }
 
-    public void completionDate(LocalDate date) {
-        task.setCompletionDate(date);
+    public static TaskBuilder forTask(String title) {
+        return new TaskBuilder(title, LocalDate.now());
     }
 
-    public void starred(boolean isStarred) {
-        task.setStarred(isStarred);
-    }
-
-    public void user(User user) {
+    public TaskBuilder user(User user) {
         task.setUser(user);
+        return this;
+    }
+
+    public TaskBuilder starred(boolean isStarred) {
+        task.setStarred(isStarred);
+        return this;
+    }
+
+    public TaskBuilder completed(boolean isCompleted) {
+        task.setCompleted(isCompleted);
+        return this;
+    }
+
+    public TaskBuilder completionDate(LocalDate completionDate) {
+        task.setCompletionDate(completionDate);
+        return this;
+    }
+
+    public TodoTask build() {
+        return task;
     }
 
 }
