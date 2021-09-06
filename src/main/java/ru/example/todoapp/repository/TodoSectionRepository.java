ed6ba369629db568a80b4fcc6466100cc315efe8
rsh-12 <grinvichforum12@mail.ru>
2021-09-06 20:12:48 +0500

    Update and delete some query methods

--- src/main/java/ru/example/todoapp/repository/TodoSectionRepository.java
+++ src/main/java/ru/example/todoapp/repository/TodoSectionRepository.java
@@ -5,18 +5,18 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import ru.example.todoapp.dto.TodoSectionDto;
 import ru.example.todoapp.entity.TodoSection;
+import ru.example.todoapp.repository.projection.TodoSectionProjection;
 
 import java.util.Optional;
 
 public interface TodoSectionRepository extends JpaRepository<TodoSection, Long> {
 
-    @Query("select new ru.example.todoapp.dto.TodoSectionDto(s.id, s.title, s.updatedAt, s.createdAt) " +
+    @Query("select new ru.example.todoapp.repository.projection.TodoSectionProjection" +
+            "(s.id, s.title, s.updatedAt, s.createdAt) " +
             "from TodoSection s where s.user.id = :userId")
-    Page<TodoSectionDto> findAllByUserIdProjection(@Param("userId") Long id, Pageable pageable);
+    Page<TodoSectionProjection> findAllByUserIdProjection(@Param("userId") Long id, Pageable pageable);
 
     Optional<TodoSection> findByUserIdAndId(Long userId, Long sectionId);
 
-    void deleteByIdAndUserId(Long sectionId, Long userId);
 }
