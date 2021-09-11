ff8101b00b76493c9cfa28654674f9d6b5cba8d7
rsh-12 <grinvichforum12@mail.ru>
2021-09-11 08:48:38 +0500

    Fix updatePassword test method

--- src/test/java/ru/example/todoapp/facade/PasswordFacadeTest.java
+++ src/test/java/ru/example/todoapp/facade/PasswordFacadeTest.java
@@ -10,16 +10,18 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import ru.example.todoapp.controller.request.TokenRequest;
+import ru.example.todoapp.entity.User;
 import ru.example.todoapp.exception.CustomException;
 import ru.example.todoapp.facade.impl.PasswordFacadeImpl;
 import ru.example.todoapp.messaging.MessagingClient;
 import ru.example.todoapp.service.UserService;
 
+import java.util.Optional;
+
 import static org.junit.Assert.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 
@@ -38,7 +40,8 @@ public class PasswordFacadeTest {
     @Test
     public void updatePassword_ShouldCallServiceMethods() {
         given(messagingService.sendTokenAndReceiveEmail(any())).willReturn("user@mail.com");
-        doNothing().when(userService).updatePassword(anyString(), anyString());
+        given(userService.updatePassword(anyString(), anyString()))
+                .willReturn(Optional.of(new User("username@mail.com", "somePassword")));
 
         passwordFacade.updatePassword(new TokenRequest("someToken"), "somePassword");
 
