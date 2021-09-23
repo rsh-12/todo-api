c87d401f9c1efd266cb9dfdc347ea1806decb2e0
rsh-12 <grinvichforum12@mail.ru>
2021-09-23 19:07:13 +0500

    Update FilterByDate enum

--- src/main/java/ru/example/todoapp/util/filters/FilterByDate.java
+++ src/main/java/ru/example/todoapp/util/filters/FilterByDate.java
@@ -1,5 +1,17 @@
 package ru.example.todoapp.util.filters;
 
 public enum FilterByDate {
-    TODAY, OVERDUE, ALL
+    TODAY("today"),
+    OVERDUE("overdue"),
+    ALL("all");
+
+    private final String VALUE;
+
+    FilterByDate(String value) {
+        VALUE = value;
+    }
+
+    public String VALUE() {
+        return VALUE;
+    }
 }
