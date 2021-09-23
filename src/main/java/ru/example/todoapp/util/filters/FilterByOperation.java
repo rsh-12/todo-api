cf04ba3c767eee4518a4a5388828f709c1448423
rsh-12 <grinvichforum12@mail.ru>
2021-09-23 19:11:42 +0500

    Update FilterByOperation enum

--- src/main/java/ru/example/todoapp/util/filters/FilterByOperation.java
+++ src/main/java/ru/example/todoapp/util/filters/FilterByOperation.java
@@ -1,5 +1,16 @@
 package ru.example.todoapp.util.filters;
 
 public enum FilterByOperation {
-    MOVE, REMOVE
+    MOVE("move"),
+    REMOVE("remove");
+
+    private final String VALUE;
+
+    FilterByOperation(String value) {
+        VALUE = value;
+    }
+
+    public String VALUE() {
+        return VALUE;
+    }
 }
