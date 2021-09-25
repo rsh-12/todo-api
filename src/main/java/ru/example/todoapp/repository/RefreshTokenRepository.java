20394ffeaf1f657c1d96ad21607a0db95bb5f38d
rsh-12 <grinvichforum12@mail.ru>
2021-09-25 09:51:17 +0500

    Add findByUserId query method

--- src/main/java/ru/example/todoapp/repository/RefreshTokenRepository.java
+++ src/main/java/ru/example/todoapp/repository/RefreshTokenRepository.java
@@ -9,4 +9,6 @@ public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long
 
     Optional<RefreshToken> findByToken(String refreshToken);
 
+    Optional<RefreshToken> findByUserId(Long userId);
+
 }
