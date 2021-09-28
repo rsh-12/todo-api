244a8c859c3443b565d31985c6ece1b685a3958c
rsh-12 <grinvichforum12@mail.ru>
2021-09-28 09:58:03 +0500

    Add findSectionById test method

--- src/test/java/ru/example/todoapp/service/TodoSectionServiceTest.java
+++ src/test/java/ru/example/todoapp/service/TodoSectionServiceTest.java
@@ -9,15 +9,19 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import ru.example.todoapp.entity.TodoSection;
 import ru.example.todoapp.facade.AuthUserFacade;
 import ru.example.todoapp.repository.TodoSectionRepository;
 import ru.example.todoapp.service.impl.TodoSectionServiceImpl;
 
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 
 @ExtendWith(SpringExtension.class)
 public class TodoSectionServiceTest {
@@ -37,6 +41,20 @@ public class TodoSectionServiceTest {
         given(authUserFacade.getUserId()).willReturn(1L);
         given(sectionRepository.findByUserIdAndId(anyLong(), anyLong())).willReturn(Optional.empty());
         assertTrue(sectionService.findSectionById(1L).isEmpty());
+    }
+
+    @Test
+    public void findSectionById_ShouldReturnSection() {
+        TodoSection section = mock(TodoSection.class);
+        given(section.getTitle()).willReturn("Title");
+
+        given(authUserFacade.getUserId()).willReturn(1L);
+        given(sectionRepository.findByUserIdAndId(anyLong(), anyLong()))
+                .willReturn(Optional.of(section));
+
+        TodoSection todoSection = sectionService.findSectionById(1L).orElse(null);
+        assertNotNull(todoSection);
+        assertEquals("Title", todoSection.getTitle());
     }
 
 }
