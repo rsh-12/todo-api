a3393e02da785a003ffeeb95dec3c14d7e6bba68
rsh-12 <grinvichforum12@mail.ru>
2021-12-28 06:22:25 +0500

    Remove unnecessary methods, add ownerOrAdmin method

--- src/main/java/ru/example/todoapp/util/Util.java
+++ src/main/java/ru/example/todoapp/util/Util.java
@@ -1,9 +1,8 @@
 package ru.example.todoapp.util;
 
-import static ru.example.todoapp.domain.Role.ADMIN;
-
 import java.util.function.Predicate;
-import ru.example.todoapp.entity.User;
+import ru.example.todoapp.entity.TodoSection;
+import ru.example.todoapp.facade.AuthUserFacade;
 
 public final class Util {
 
@@ -11,14 +10,9 @@ public final class Util {
         throw new AssertionError();
     }
 
-    public static Predicate<User> checkUserAccess(User candidate) {
-        Predicate<User> predicate = user -> user.equals(candidate);
-        return predicate.or(user -> user.getRoles().contains(ADMIN));
-    }
-
-    public static Predicate<User> authorizeUser(Long id) {
-        Predicate<User> predicate = user -> user.getId().equals(id);
-        return predicate.or(user -> user.getRoles().contains(ADMIN));
+    public static Predicate<TodoSection> ownerOrAdmin(AuthUserFacade currentUser) {
+        return s -> s.getUserId().equals(currentUser.getId())
+                || currentUser.containsRoleAdmin();
     }
 
 }
