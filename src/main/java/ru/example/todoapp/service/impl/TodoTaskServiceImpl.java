07d5d9524c283a52bf7c1a9f5554d439d4b10ca2
rsh-12 <grinvichforum12@mail.ru>
2021-12-29 09:17:50 +0500

    Update TodoTaskServiceImpl.java

--- src/main/java/ru/example/todoapp/service/impl/TodoTaskServiceImpl.java
+++ src/main/java/ru/example/todoapp/service/impl/TodoTaskServiceImpl.java
@@ -4,6 +4,13 @@ package ru.example.todoapp.service.impl;
  * Time: 6:16 PM
  * */
 
+import static ru.example.todoapp.util.filters.FilterByDate.OVERDUE;
+import static ru.example.todoapp.util.filters.FilterByDate.TODAY;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,20 +21,12 @@ import org.springframework.transaction.annotation.Transactional;
 import ru.example.todoapp.domain.request.TodoTaskRequest;
 import ru.example.todoapp.dsl.TaskBuilder;
 import ru.example.todoapp.entity.TodoTask;
-import ru.example.todoapp.exception.CustomException;
 import ru.example.todoapp.facade.AuthUserFacade;
 import ru.example.todoapp.repository.TodoTaskRepository;
 import ru.example.todoapp.service.TodoTaskService;
-import ru.example.todoapp.util.Combinators;
+import ru.example.todoapp.service.UserService;
+import ru.example.todoapp.util.ServiceUtil;
 import ru.example.todoapp.util.filters.FilterByDate;
-
-import java.time.LocalDate;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
-
-import static ru.example.todoapp.util.filters.FilterByDate.OVERDUE;
-import static ru.example.todoapp.util.filters.FilterByDate.TODAY;
 
 @Service
 @Transactional
@@ -35,27 +34,41 @@ public class TodoTaskServiceImpl implements TodoTaskService {
 
     private static final Logger log = LoggerFactory.getLogger(TodoTaskServiceImpl.class);
 
-    private final TodoTaskRepository todoTaskRepository;
-    private final AuthUserFacade authUserFacade;
+    private final TodoTaskRepository taskRepository;
+    private final AuthUserFacade currentUser;
+    private final UserService userService;
+    private final ServiceUtil serviceUtil;
 
     @Autowired
-    public TodoTaskServiceImpl(TodoTaskRepository todoTaskRepository, AuthUserFacade authUserFacade) {
-        this.todoTaskRepository = todoTaskRepository;
-        this.authUserFacade = authUserFacade;
+    public TodoTaskServiceImpl(
+            TodoTaskRepository taskRepository,
+            AuthUserFacade currentUser,
+            UserService userService,
+            ServiceUtil serviceUtil) {
+
+        this.taskRepository = taskRepository;
+        this.currentUser = currentUser;
+        this.userService = userService;
+        this.serviceUtil = serviceUtil;
     }
 
     // get all tasks
     @Override
     @Transactional(readOnly = true)
     public Page<TodoTask> findAll(FilterByDate date, Pageable pageable) {
-        Long userId = authUserFacade.getUserId();
+        Long userId = currentUser.getId();
 
         if (date == OVERDUE) {
-            return todoTaskRepository.findAllByCompletionDateBeforeAndUserId(LocalDate.now(), userId, pageable);
+            log.info("findAll: getting overdue tasks");
+            return taskRepository.findAllByCompletionDateBeforeAndUserId(
+                    LocalDate.now(), userId, pageable);
         } else if (date == TODAY) {
-            return todoTaskRepository.findAllByCompletionDateEqualsAndUserId(LocalDate.now(), userId, pageable);
+            log.info("findAll: getting current tasks");
+            return taskRepository.findAllByCompletionDateEqualsAndUserId(
+                    LocalDate.now(), userId, pageable);
         } else {
-            return todoTaskRepository.findAllByUserId(userId, pageable);
+            log.info("findAll: getting all tasks");
+            return taskRepository.findAllByUserId(userId, pageable);
         }
     }
 
@@ -63,49 +76,55 @@ public class TodoTaskServiceImpl implements TodoTaskService {
     @Override
     @Transactional(readOnly = true)
     public Optional<TodoTask> findOne(Long taskId) {
-        log.info("Get the task by id: {}", taskId);
-        return todoTaskRepository.findByIdAndUserId(taskId, authUserFacade.getUserId());
+        log.info("findOne: getting the TodoTask with ID={}", taskId);
+
+        return currentUser.containsRoleAdmin()
+                ? taskRepository.findById(taskId)
+                : taskRepository.findByIdAndUserId(taskId, currentUser.getId());
     }
 
     // delete task by id
     @Override
     public void delete(Long taskId) {
-        todoTaskRepository.findById(taskId).map(TodoTask::getUser)
-                .filter(Combinators.checkUserAccess(authUserFacade.getLoggedUser()))
-                .ifPresentOrElse(user -> todoTaskRepository.deleteById(taskId), () -> {
-                    throw CustomException.createNotFoundExc("Task not found");
-                });
+        log.info("delete: deleting a TodoTask with ID={}", taskId);
+
+        taskRepository.findById(taskId)
+                .map(TodoTask::getUserId)
+                .filter(serviceUtil::ownerOrAdmin)
+                .ifPresent(taskRepository::deleteById);
     }
 
     // create new task
     @Override
     public TodoTask create(TodoTaskRequest request) {
-        log.info("Create a new task");
+        log.info("create: creating a new TodoTask");
+
         TodoTask todoTask = TaskBuilder.forTask(request.title())
                 .starred(request.starred())
                 .completionDate(request.completionDate())
-                .user(authUserFacade.getLoggedUser())
                 .build();
 
-        return todoTaskRepository.save(todoTask);
+        userService.findOne(currentUser.getId()).ifPresent(todoTask::setUser);
+
+        return taskRepository.save(todoTask);
     }
 
     @Override
     @Transactional(readOnly = true)
     public List<TodoTask> findByIds(Set<Long> taskIds, Long userId) {
-        List<TodoTask> tasksByIds = todoTaskRepository.findAllByIdInAndUserId(taskIds, userId);
-        log.info("Get tasks by set of ids: {}", tasksByIds.size());
-        return tasksByIds;
+        log.info("findByIds: getting tasks by IDS={}", taskIds);
+        return taskRepository.findAllByIdInAndUserId(taskIds, userId);
     }
 
     @Override
-    public Optional<TodoTask> save(Long taskId, TodoTaskRequest request) {
+    public Optional<TodoTask> update(Long taskId, TodoTaskRequest request) {
+        log.info("update: updating the task with ID={}", taskId);
         return findOne(taskId)
                 .map(task -> {
                     task.setTitle(request.title());
                     task.setStarred(request.starred());
                     task.setCompletionDate(request.completionDate());
-                    return todoTaskRepository.save(task);
+                    return taskRepository.save(task);
                 });
     }
 
