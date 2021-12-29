427bac2d8b267c5b46a860211b1cd645c5b9ccaa
rsh-12 <grinvichforum12@mail.ru>
2021-12-29 09:16:45 +0500

    Update TodoSectionServiceImpl

--- src/main/java/ru/example/todoapp/service/impl/TodoSectionServiceImpl.java
+++ src/main/java/ru/example/todoapp/service/impl/TodoSectionServiceImpl.java
@@ -4,8 +4,10 @@ package ru.example.todoapp.service.impl;
  * Time: 6:16 PM
  * */
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.List;
+import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
@@ -16,27 +18,34 @@ import org.springframework.transaction.annotation.Transactional;
 import ru.example.todoapp.domain.request.TodoSectionRequest;
 import ru.example.todoapp.entity.TodoSection;
 import ru.example.todoapp.entity.TodoTask;
-import ru.example.todoapp.exception.CustomException;
 import ru.example.todoapp.facade.AuthUserFacade;
 import ru.example.todoapp.repository.TodoSectionRepository;
 import ru.example.todoapp.service.TodoSectionService;
-import ru.example.todoapp.util.Combinators;
+import ru.example.todoapp.service.UserService;
+import ru.example.todoapp.util.ServiceUtil;
 
-import java.util.List;
-import java.util.Optional;
 
+@Slf4j
 @Service
 @Transactional
 public class TodoSectionServiceImpl implements TodoSectionService {
 
-    private static final Logger log = LoggerFactory.getLogger(TodoSectionServiceImpl.class);
+    private final TodoSectionRepository sectionRepository;
+    private final AuthUserFacade currentUser;
+    private final UserService userService;
+    private final ServiceUtil serviceUtil;
 
-    private final TodoSectionRepository todoSectionRepository;
-    private final AuthUserFacade authUserFacade;
+    @Autowired
+    public TodoSectionServiceImpl(
+            TodoSectionRepository sectionRepository,
+            AuthUserFacade currentUser,
+            UserService userService,
+            ServiceUtil serviceUtil) {
 
-    public TodoSectionServiceImpl(TodoSectionRepository todoSectionRepository, AuthUserFacade authUserFacade) {
-        this.todoSectionRepository = todoSectionRepository;
-        this.authUserFacade = authUserFacade;
+        this.sectionRepository = sectionRepository;
+        this.currentUser = currentUser;
+        this.userService = userService;
+        this.serviceUtil = serviceUtil;
     }
 
     // get section by id
@@ -44,71 +53,75 @@ public class TodoSectionServiceImpl implements TodoSectionService {
     @Cacheable(cacheNames = "sections", key = "#sectionId")
     @Transactional(readOnly = true)
     public Optional<TodoSection> findOne(Long sectionId) {
-        log.info("Get the section by id: {}", sectionId);
-
-        return todoSectionRepository.findByUserIdAndId(authUserFacade.getUserId(), sectionId);
+        log.info("findOne: getting a section with ID: {}", sectionId);
+        return sectionRepository.findById(sectionId)
+                .filter(section -> serviceUtil.ownerOrAdmin(section.getUserId()));
     }
 
     // get all sections
     @Override
     @Transactional(readOnly = true)
     public Page<TodoSection> findAll(Pageable pageable) {
-        Long userId = authUserFacade.getUserId();
-        return todoSectionRepository.findAllByUserId(userId, pageable);
+        if (currentUser.containsRoleAdmin()) {
+            log.info("findAll: getting all sections");
+            return sectionRepository.findAll(pageable);
+        }
+
+        Long userId = currentUser.getId();
+        log.info("findAll: getting all sections by userId: {}", userId);
+
+        return sectionRepository.findAllByUserId(userId, pageable);
     }
 
     // delete section by id
     @Override
-    @CacheEvict(cacheNames = "sections", key = "#id")
-    public void delete(Long id) {
-        todoSectionRepository.findById(id).map(TodoSection::getUser)
-                .filter(Combinators.checkUserAccess(authUserFacade.getLoggedUser()))
-                .ifPresentOrElse(user -> todoSectionRepository.deleteById(id), () -> {
-                    throw CustomException.createNotFoundExc("Section not found");
-                });
+    @CacheEvict(cacheNames = "sections", key = "#sectionId")
+    public void delete(Long sectionId) {
+        log.info("delete: deleting a section with ID: {}", sectionId);
+
+        sectionRepository.findById(sectionId)
+                .map(TodoSection::getUserId)
+                .filter(serviceUtil::ownerOrAdmin)
+                .ifPresent(sectionRepository::deleteById);
     }
 
     // create new section
     @Override
     public TodoSection create(TodoSectionRequest sectionRequest) {
-        TodoSection section = new TodoSection(sectionRequest.getTitle());
-        section.setUser(authUserFacade.getLoggedUser());
-        log.info("Create a new section");
-        return todoSectionRepository.save(section);
+        log.info("create: creating a new section");
+        throw new AssertionError("Not implemented");
     }
 
     // update section title
     @Override
     @CachePut(cacheNames = "sections", key = "#sectionId")
     public Optional<TodoSection> update(Long sectionId, TodoSectionRequest sectionRequest) {
-        Optional<TodoSection> sectionOptional = todoSectionRepository.findById(sectionId);
+        log.info("update: updating the section with ID: {}", sectionId);
 
-        return sectionOptional.map(TodoSection::getUser)
-                .filter(Combinators.checkUserAccess(authUserFacade.getLoggedUser()))
-                .map(user -> {
-                    TodoSection section = sectionOptional.get();
-                    section.setUser(user);
+        return sectionRepository.findById(sectionId)
+                .filter(section -> serviceUtil.ownerOrAdmin(section.getUserId()))
+                .map(section -> {
                     section.setTitle(sectionRequest.getTitle());
-                    return todoSectionRepository.save(section);
+                    return section;
                 });
     }
 
     // add to or remove from the task section
     @Override
     public void addTasks(Long userId, Long sectionId, List<TodoTask> tasks) {
-        todoSectionRepository.findByUserIdAndId(userId, sectionId)
+        sectionRepository.findByUserIdAndId(userId, sectionId)
                 .ifPresent(section -> {
                     tasks.forEach(section::addTodoTask);
-                    todoSectionRepository.save(section);
+                    sectionRepository.save(section);
                 });
     }
 
     @Override
     public void removeTasks(Long userId, Long sectionId, List<TodoTask> tasks) {
-        todoSectionRepository.findByUserIdAndId(userId, sectionId)
+        sectionRepository.findByUserIdAndId(userId, sectionId)
                 .ifPresent(section -> {
                     section.removeTodoTasks(tasks);
-                    todoSectionRepository.save(section);
+                    sectionRepository.save(section);
                 });
     }
 
