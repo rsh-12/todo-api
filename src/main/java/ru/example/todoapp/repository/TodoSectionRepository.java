4496433779f5adbac2090cbdb3539d8203d6c116
rsh-12 <grinvichforum12@mail.ru>
2021-12-29 08:33:46 +0500

    Update TodoSectionRepository.java

--- src/main/java/ru/example/todoapp/repository/TodoSectionRepository.java
+++ src/main/java/ru/example/todoapp/repository/TodoSectionRepository.java
@@ -1,12 +1,11 @@
 package ru.example.todoapp.repository;
 
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import ru.example.todoapp.entity.TodoSection;
-
-import java.util.Optional;
 
 public interface TodoSectionRepository extends JpaRepository<TodoSection, Long> {
 
