0a5d0d9fed2670ab4d3ede8e8fe705aad01d6c4c
rsh-12 <grinvichforum12@mail.ru>
2022-03-21 23:24:32 +0500

    Remove commented test method

--- src/test/java/ru/example/todoapp/facade/TasksFacadeTest.java
+++ src/test/java/ru/example/todoapp/facade/TasksFacadeTest.java
@@ -4,30 +4,19 @@ package ru.example.todoapp.facade;
  * Time: 3:20 AM
  * */
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verifyNoInteractions;
+
+import java.util.Collections;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import ru.example.todoapp.entity.TodoTask;
-import ru.example.todoapp.exception.CustomException;
+import ru.example.todoapp.exception.BadRequestException;
 import ru.example.todoapp.facade.impl.TasksFacadeImpl;
 import ru.example.todoapp.service.TodoSectionService;
 import ru.example.todoapp.service.TodoTaskService;
-
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
-
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.anyList;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.ArgumentMatchers.anySet;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoInteractions;
 
 @ExtendWith(SpringExtension.class)
 public class TasksFacadeTest {
@@ -44,28 +33,12 @@ public class TasksFacadeTest {
     @Mock
     private TodoSectionService sectionService;
 
-/*
-    @Test
-    public void addTasks_ShouldCallServiceMethods() {
-        TodoTask task1 = mock(TodoTask.class);
-        TodoTask task2 = mock(TodoTask.class);
-
-        given(authUserFacade.getUserId()).willReturn(1L);
-        given(taskService.findByIds(anySet(), anyLong())).willReturn(List.of(task1, task2));
-        doNothing().when(sectionService).addTasks(anyLong(), anyLong(), anyList());
-
-        tasksFacade.addTasks(1L, Set.of(1L, 2L));
-
-        verify(taskService).findByIds(anySet(), anyLong());
-        verify(sectionService).addTasks(anyLong(), anyLong(), anyList());
-    }
-*/
-
     @Test
     public void addTasks_ShouldThrowCustomException() {
-        assertThrows(CustomException.class, () -> tasksFacade.addTasks(1L, Collections.emptySet()));
+        assertThrows(BadRequestException.class, () -> tasksFacade.addTasks(1L, Collections.emptySet()));
 
         verifyNoInteractions(taskService);
         verifyNoInteractions(sectionService);
     }
 }
+
